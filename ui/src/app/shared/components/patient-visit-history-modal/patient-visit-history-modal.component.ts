293b05da85d8409c506c6e79278df8308ec3339b
Bennett <bennybenester@gmail.com>
2021-10-30 18:35:53 +0300

    show vitals history in nursing

--- ui/src/app/shared/components/patient-visit-history-modal/patient-visit-history-modal.component.ts
+++ ui/src/app/shared/components/patient-visit-history-modal/patient-visit-history-modal.component.ts
@@ -1,23 +1,24 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { AppState } from 'src/app/store/reducers';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { AppState } from "src/app/store/reducers";
 import {
   getAllForms,
   getOpenMRSForms,
-} from 'src/app/store/selectors/form.selectors';
-import { VisitsService } from '../../resources/visits/services';
+} from "src/app/store/selectors/form.selectors";
+import { VisitsService } from "../../resources/visits/services";
 
 @Component({
-  selector: 'app-patient-visit-history-modal',
-  templateUrl: './patient-visit-history-modal.component.html',
-  styleUrls: ['./patient-visit-history-modal.component.scss'],
+  selector: "app-patient-visit-history-modal",
+  templateUrl: "./patient-visit-history-modal.component.html",
+  styleUrls: ["./patient-visit-history-modal.component.scss"],
 })
 export class PatientVisitHistoryModalComponent implements OnInit {
   visits: any[];
   patientVisits$: Observable<any>;
   patientUuid: string;
+  location: Boolean;
   omitCurrent: boolean = true;
   forms$: Observable<any[]>;
   constructor(
@@ -27,6 +28,11 @@ export class PatientVisitHistoryModalComponent implements OnInit {
     private store: Store<AppState>
   ) {
     this.patientUuid = data?.patientUuid;
+    this.location =
+      data?.location?.tags &&
+      data?.location?.tags.find(({ name }) => name === "Triage Location")
+        ? true
+        : false;
   }
 
   ngOnInit(): void {
