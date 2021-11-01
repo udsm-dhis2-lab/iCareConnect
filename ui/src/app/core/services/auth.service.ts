99ce008ba316c5f5d3a987368f81d7e00fcce5bf
rajey <rajeyw@gmail.com>
2021-11-01 16:27:32 +0300

    resolve issues in clearing cookie

--- ui/src/app/core/services/auth.service.ts
+++ ui/src/app/core/services/auth.service.ts
@@ -47,13 +47,38 @@ export class AuthService {
     return this._session;
   }
 
+  clearCookie() {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+      var d = window.location.hostname.split(".");
+      while (d.length > 0) {
+        var cookieBase =
+          encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) +
+          "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=" +
+          d.join(".") +
+          " ;path=";
+        var p = location.pathname.split("/");
+        document.cookie = cookieBase + "/";
+        while (p.length > 0) {
+          document.cookie = cookieBase + p.join("/");
+          p.pop();
+        }
+        d.shift();
+      }
+    }
+  }
+
   login(credentialsToken: string): Observable<any> {
     localStorage.clear();
     window.localStorage.clear();
     sessionStorage.clear();
+
+    this.clearCookie();
+
     return this.httpClient.login(credentialsToken).pipe(
       switchMap((loginResponse) => {
         const { authenticated, user } = loginResponse;
+        this._session.next(loginResponse);
         return this.currentUserService.get(user?.uuid).pipe(
           map((userDetails) => {
             const authDetails = {
@@ -80,6 +105,7 @@ export class AuthService {
   }
 
   logout(): Observable<any> {
+    this.clearCookie();
     const headers = {
       httpHeaders: {
         Authorization: "Basic " + localStorage.getItem("credentialsToken"),
