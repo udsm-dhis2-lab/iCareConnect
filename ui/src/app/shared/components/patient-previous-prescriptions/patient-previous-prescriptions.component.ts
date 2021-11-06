34d1ff9c10aa8000e77b6afac26810f24232fcab
josephatj <josephatjulius24@gmail.com>
2021-11-06 06:07:15 +0300

    add prescription support

--- ui/src/app/shared/components/patient-previous-prescriptions/patient-previous-prescriptions.component.ts
+++ ui/src/app/shared/components/patient-previous-prescriptions/patient-previous-prescriptions.component.ts
@@ -1,24 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Patient } from '../../resources/patient/models/patient.model';
-import { VisitObject } from '../../resources/visits/models/visit-object.model';
-import { Visit } from '../../resources/visits/models/visit.model';
-import { VisitsService } from '../../resources/visits/services';
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Patient } from "../../resources/patient/models/patient.model";
+import { VisitObject } from "../../resources/visits/models/visit-object.model";
+import { Visit } from "../../resources/visits/models/visit.model";
+import { VisitsService } from "../../resources/visits/services";
 
 @Component({
-  selector: 'app-patient-previous-prescriptions',
-  templateUrl: './patient-previous-prescriptions.component.html',
-  styleUrls: ['./patient-previous-prescriptions.component.scss'],
+  selector: "app-patient-previous-prescriptions",
+  templateUrl: "./patient-previous-prescriptions.component.html",
+  styleUrls: ["./patient-previous-prescriptions.component.scss"],
 })
 export class PatientPreviousPrescriptionsComponent implements OnInit {
   @Input() patient: Patient;
+  @Input() patientUuid: string;
   previousVisitDetails$: Observable<any>;
   constructor(private visitService: VisitsService) {}
 
   ngOnInit(): void {
     this.previousVisitDetails$ = this.visitService
-      .getLastPatientVisit(this.patient['patient']?.uuid, true)
+      .getLastPatientVisit(this.patientUuid, true)
       .pipe(
         map((response) => {
           return response;
