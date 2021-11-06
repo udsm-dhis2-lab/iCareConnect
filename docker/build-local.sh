de3f5355f6ffeef087e3194433cd08bc6ae002cd
Vincent Minde <vincentminde@gmail.com>
2021-11-06 15:36:04 +0300

    Fixed build versioning

--- docker/build-local.sh
+++ docker/build-local.sh
@@ -4,5 +4,4 @@ docker run -w="/app" -v "$(pwd)":/app node:12.18.3-alpine3.12 npm run build:prod
 cd ..
 docker run --rm -v maven-repo:/root/.m2 -v $(pwd)/omods/core:/usr/src/omod -w /usr/src/omod maven:3.6.3-openjdk-11-slim mvn clean package -DskipTests
 version=$(cat version)
-echo "$version"
 docker build -t udsmdhis2/icare-core:$version .
