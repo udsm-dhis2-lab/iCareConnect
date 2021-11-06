2191b7b840e6cbef425e3a51af32497b6ecf9840
josephatj <josephatjulius24@gmail.com>
2021-11-06 09:57:19 +0300

    add support to render vitals from visit history details

--- ui/src/app/modules/nursing/pages/nursing-home/nursing-home.component.ts
+++ ui/src/app/modules/nursing/pages/nursing-home/nursing-home.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute } from '@angular/router';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { PatientListDialogComponent } from 'src/app/shared/dialogs';
-import { Patient } from 'src/app/shared/resources/patient/models/patient.model';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { ActivatedRoute } from "@angular/router";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { PatientListDialogComponent } from "src/app/shared/dialogs";
+import { Patient } from "src/app/shared/resources/patient/models/patient.model";
 import {
   addCurrentPatient,
   authenticateUser,
   go,
   loadRolesDetails,
-} from 'src/app/store/actions';
-import { AppState } from 'src/app/store/reducers';
-import { getSettingCurrentLocationStatus } from 'src/app/store/selectors';
-import { getVisitLoadingState } from 'src/app/store/selectors/visit.selectors';
+} from "src/app/store/actions";
+import { AppState } from "src/app/store/reducers";
+import { getSettingCurrentLocationStatus } from "src/app/store/selectors";
+import { getVisitLoadingState } from "src/app/store/selectors/visit.selectors";
 
 @Component({
-  selector: 'app-nursing-home',
-  templateUrl: './nursing-home.component.html',
-  styleUrls: ['./nursing-home.component.scss'],
+  selector: "app-nursing-home",
+  templateUrl: "./nursing-home.component.html",
+  styleUrls: ["./nursing-home.component.scss"],
 })
 export class NursingHomeComponent implements OnInit {
   loadingVisit$: Observable<boolean>;
-  isPatientListTabular: boolean = true;
+  isPatientListTabular: boolean;
   settingCurrentLocationStatus$: Observable<boolean>;
   constructor(
     private store: Store<AppState>,
@@ -36,21 +36,23 @@ export class NursingHomeComponent implements OnInit {
     this.settingCurrentLocationStatus$ = this.store.select(
       getSettingCurrentLocationStatus
     );
-    this.isPatientListTabular = this.route.snapshot.queryParams['list']
+    this.isPatientListTabular = this.route.snapshot.queryParams["tabular"]
       ? true
-      : false;
+      : this.route.snapshot.queryParams["account_box"]
+      ? false
+      : true;
     this.loadingVisit$ = this.store.pipe(select(getVisitLoadingState));
   }
 
   onSearchPatient(e: Event) {
     const patientListDialog = this.dialog.open(PatientListDialogComponent, {
-      width: '800px',
+      width: "800px",
     });
 
     patientListDialog
       .afterClosed()
       .subscribe((response: { action: string; patient: Patient }) => {
-        if (response?.action === 'PATIENT_SELECT') {
+        if (response?.action === "PATIENT_SELECT") {
           this.store.dispatch(addCurrentPatient({ patient: response.patient }));
         }
       });
@@ -63,7 +65,7 @@ export class NursingHomeComponent implements OnInit {
     this.store.dispatch(addCurrentPatient({ patient }));
     this.store.dispatch(
       go({
-        path: ['/nursing/consult'],
+        path: ["/nursing/consult"],
         query: { queryParams: { patient: patient?.patient?.uuid } },
       })
     );
@@ -71,6 +73,6 @@ export class NursingHomeComponent implements OnInit {
 
   onBack(e: Event) {
     e.stopPropagation();
-    this.store.dispatch(go({ path: ['/'] }));
+    this.store.dispatch(go({ path: ["/"] }));
   }
 }
--- ui/src/app/shared/components/patient-vitals-summary/patient-vitals-summary.component.ts
+++ ui/src/app/shared/components/patient-vitals-summary/patient-vitals-summary.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { AppState } from 'src/app/store/reducers';
+import { Component, Input, OnInit } from "@angular/core";
+import { select, Store } from "@ngrx/store";
+import { Observable, of } from "rxjs";
+import { getVitalsFromVisitDetails } from "src/app/core";
+import { AppState } from "src/app/store/reducers";
 import {
   getGroupedObservationByConcept,
   getVitalSignObservations,
-} from 'src/app/store/selectors/observation.selectors';
-import { getVisitLoadingState } from 'src/app/store/selectors/visit.selectors';
+} from "src/app/store/selectors/observation.selectors";
+import { getVisitLoadingState } from "src/app/store/selectors/visit.selectors";
 
 @Component({
-  selector: 'app-patient-vitals-summary',
-  templateUrl: './patient-vitals-summary.component.html',
-  styleUrls: ['./patient-vitals-summary.component.scss'],
+  selector: "app-patient-vitals-summary",
+  templateUrl: "./patient-vitals-summary.component.html",
+  styleUrls: ["./patient-vitals-summary.component.scss"],
 })
 export class PatientVitalsSummaryComponent implements OnInit {
+  @Input() patientVisit: any;
+  @Input() vitalsForm: any;
   observationsGroupedByConcept$: Observable<any>;
   loadingVisit$: Observable<boolean>;
   vitalSignObservations$: Observable<any>;
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.vitalSignObservations$ = this.store.pipe(
-      select(getVitalSignObservations)
-    );
+    const observations = this.patientVisit?.observations;
+    const vitalsData = getVitalsFromVisitDetails(this.vitalsForm, observations);
+
+    this.vitalSignObservations$ = of(vitalsData);
     this.observationsGroupedByConcept$ = this.store.select(
       getGroupedObservationByConcept
     );
