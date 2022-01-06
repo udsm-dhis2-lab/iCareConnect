856f06aea3b8a4530bf4ee3bbfe609947369c386
josephatJ <josephatjulius24@gmail.com>
2022-01-06 16:51:41 +0300

    add service for saving constructed messages

---
+++ ui/src/app/core/services/sms.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { OpenmrsHttpClientService } from 'src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SmsService {
+
+  constructor(private httpClientService: OpenmrsHttpClientService) { }
+
+  uploadMessages(data): Observable<any> {
+    return this.httpClientService.post('messages', data).pipe(map(response => response),catchError(error=> of(error)))
+  }
+}
