7633bfa4fed0e3515c21e37c012b31f7fd08c385
josephatJ <josephatjulius24@gmail.com>
2022-02-04 13:19:40 +0300

    Add support to save obs with respective order Fix bill checking issue

---
+++ ui/src/app/shared/modules/location-tree/containers/location-tree/location-tree.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "ngx-location-tree",
+  templateUrl: "./location-tree.component.html",
+  styleUrls: ["./location-tree.component.scss"],
+})
+export class LocationTreeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
