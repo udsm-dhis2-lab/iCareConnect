b52ad239e9f731cc457a8d7bb701fd1e9bffc1aa
josephatJ <josephatjulius24@gmail.com>
2022-04-27 10:34:19 +0300

    Fix edit patient issue

--- ui/src/app/modules/registration/pages/patient-edit/patient-edit.component.ts
+++ ui/src/app/modules/registration/pages/patient-edit/patient-edit.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { patientObj } from 'src/app/shared/models/patient';
-import { OpenmrsHttpClientService } from 'src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service';
-import { PatientGetFull } from 'src/app/shared/resources/openmrs';
-import { Patient } from 'src/app/shared/resources/patient/models/patient.model';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { patientObj } from "src/app/shared/models/patient";
+import { OpenmrsHttpClientService } from "src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service";
+import { PatientGetFull } from "src/app/shared/resources/openmrs";
+import { Patient } from "src/app/shared/resources/patient/models/patient.model";
+import { RegistrationService } from "../../services/registration.services";
 
 @Component({
-  selector: 'app-patient-edit',
-  templateUrl: './patient-edit.component.html',
-  styleUrls: ['./patient-edit.component.scss'],
+  selector: "app-patient-edit",
+  templateUrl: "./patient-edit.component.html",
+  styleUrls: ["./patient-edit.component.scss"],
 })
 export class PatientEditComponent implements OnInit {
   patientDetails$: Observable<Patient>;
   patientId: string;
+  registrationConfigurations$: Observable<any>;
+
   constructor(
     private store: Store,
     private openmrService: OpenmrsHttpClientService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private registrationService: RegistrationService
   ) {}
 
   ngOnInit(): void {
-    this.patientId = this.route.snapshot.params['patientId'];
+    this.patientId = this.route.snapshot.params["patientId"];
     this.patientDetails$ = this.openmrService
       .get(`patient/${this.patientId}?v=full`)
       .pipe(
@@ -31,5 +35,7 @@ export class PatientEditComponent implements OnInit {
           return new Patient(response);
         })
       );
+    this.registrationConfigurations$ =
+      this.registrationService.getRegistrationConfigurations();
   }
 }
