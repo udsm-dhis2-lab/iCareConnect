b5af50563b570455052994d7d029956be378ec62
Jesse-Justin Mdachi <jessejustinm@gmail.com>
2022-05-02 17:22:15 +0300

    fix login popup

--- ui/src/app/core/services/location.service.ts
+++ ui/src/app/core/services/location.service.ts
@@ -1,35 +1,39 @@
-import { Injectable } from '@angular/core';
-import { OpenmrsHttpClientService } from 'src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { head } from 'lodash';
+import { Injectable } from "@angular/core";
+import { OpenmrsHttpClientService } from "src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { head } from "lodash";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class LocationService {
   constructor(private httpClient: OpenmrsHttpClientService) {}
 
   getLoginLocations(): Observable<any> {
     return this.httpClient.get(
-      'location?limit=100&tag=Login+Location&v=custom:(display,uuid,tags,description,parentLocation,childLocations,attributes:(attributeType,uuid,value,display))'
+      "location?limit=100&tag=Login+Location&v=custom:(display,uuid,tags,description,parentLocation,childLocations,attributes:(attributeType,uuid,value,display))"
     );
   }
 
   getLocationById(uuid): Observable<any> {
-    return this.httpClient.get('location/' + uuid + '?v=full');
+    return this.httpClient.get("location/" + uuid + "?v=full");
   }
 
   getAllLocations() {
-    return this.httpClient.get('location?v=full&limit=100');
+    return this.httpClient.get("location?v=full&limit=100");
+  }
+
+  getAllLocationsByLoginLocationTag() {
+    return this.httpClient.get("location?v=full&limit=100&tag=Login+Location");
   }
 
   getLocationsByTagName(tagName): Observable<any[]> {
-    return this.httpClient.get('location?tag=' + tagName + '&v=full&limit=100');
+    return this.httpClient.get("location?tag=" + tagName + "&v=full&limit=100");
   }
 
   getFacilityCode(): Observable<any> {
-    return this.httpClient.get('systemsetting?q=facility.code&v=full').pipe(
+    return this.httpClient.get("systemsetting?q=facility.code&v=full").pipe(
       map((res: any) => {
         return head((res?.results || []).map((payload) => payload?.value));
       })
