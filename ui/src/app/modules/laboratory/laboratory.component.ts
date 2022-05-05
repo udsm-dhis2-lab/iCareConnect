a496558e06db27703f9dd4ab4654d9fb7381068f
josephatJ <josephatjulius24@gmail.com>
2022-05-05 08:20:54 +0300

    Add dates range difference to handle the difference

--- ui/src/app/modules/laboratory/laboratory.component.ts
+++ ui/src/app/modules/laboratory/laboratory.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { map, take } from 'rxjs/operators';
-import { AppState } from 'src/app/store/reducers';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Store } from "@ngrx/store";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
+import { map, take } from "rxjs/operators";
+import { AppState } from "src/app/store/reducers";
 import {
   getAllUSerRoles,
   getCurrentUserInfo,
   getCurrentUserPrivileges,
-} from 'src/app/store/selectors/current-user.selectors';
-import { formatDateToYYMMDD } from 'src/app/shared/helpers/format-date.helper';
+} from "src/app/store/selectors/current-user.selectors";
+import { formatDateToYYMMDD } from "src/app/shared/helpers/format-date.helper";
 import {
   loadLabConfigurations,
   setVisitsParameters,
@@ -21,17 +21,17 @@ import {
   clearLoadedLabOrders,
   loadRolesDetails,
   loadOrderTypes,
-} from 'src/app/store/actions';
-import { loadSpecimenSources } from './store/actions/specimen-sources-and-tests-management.actions';
-import { getAllSampleTypes } from 'src/app/store/selectors';
+} from "src/app/store/actions";
+import { loadSpecimenSources } from "./store/actions/specimen-sources-and-tests-management.actions";
+import { getAllSampleTypes } from "src/app/store/selectors";
 
 @Component({
-  selector: 'lab-root',
-  templateUrl: './laboratory.component.html',
-  styleUrls: ['./laboratory.component.scss'],
+  selector: "lab-root",
+  templateUrl: "./laboratory.component.html",
+  styleUrls: ["./laboratory.component.scss"],
 })
 export class LaboratoryComponent implements OnInit {
-  title = 'Laboratory';
+  title = "Laboratory";
   billingInformation$: Observable<any>;
   results$: Observable<any>;
   labOrders$: Observable<any>;
@@ -48,6 +48,7 @@ export class LaboratoryComponent implements OnInit {
   showDate: Boolean = true;
   startDate: any;
   endDate: any;
+  datesRangeDifference: number = 5;
   currentSubModule: string;
 
   sampleTypes$: Observable<any>;
@@ -66,33 +67,33 @@ export class LaboratoryComponent implements OnInit {
       if (currentRoute instanceof NavigationEnd) {
         // console.log(currentRoute);
 
-        if (currentRoute?.url?.includes('/sample-acceptance-and-results')) {
-          this.enableDate(1);
-          this.currentSubModule = 'acceptance';
-        } else if (currentRoute?.url?.includes('/sample-tracking')) {
-          this.enableDate(2);
-          this.currentSubModule = 'tracking';
-        } else if (currentRoute?.url?.includes('/lab-investigation-home')) {
+        if (currentRoute?.url?.includes("/sample-acceptance-and-results")) {
+          this.enableDate(this.datesRangeDifference);
+          this.currentSubModule = "acceptance";
+        } else if (currentRoute?.url?.includes("/sample-tracking")) {
+          this.enableDate(this.datesRangeDifference);
+          this.currentSubModule = "tracking";
+        } else if (currentRoute?.url?.includes("/lab-investigation-home")) {
           this.disableDate();
-          this.currentSubModule = 'order-tests';
-        } else if (currentRoute?.url?.includes('/reports')) {
+          this.currentSubModule = "order-tests";
+        } else if (currentRoute?.url?.includes("/reports")) {
           this.disableDate();
-          this.currentSubModule = 'reports';
-        } else if (currentRoute?.url?.includes('/settings')) {
+          this.currentSubModule = "reports";
+        } else if (currentRoute?.url?.includes("/settings")) {
           this.disableDate();
-          this.currentSubModule = 'settings';
-        } else if (currentRoute?.url?.includes('/sample-collection')) {
-          this.currentSubModule = 'collection';
+          this.currentSubModule = "settings";
+        } else if (currentRoute?.url?.includes("/sample-collection")) {
+          this.currentSubModule = "collection";
           this.disableDate();
-        } else if (currentRoute?.url === '/laboratory') {
-          this.router.navigate(['laboratory']);
+        } else if (currentRoute?.url === "/laboratory") {
+          this.router.navigate(["laboratory"]);
         }
       }
     });
   }
 
   ngOnInit() {
-    this.url$ = this.route.url.pipe(map((segments) => segments.join('')));
+    this.url$ = this.route.url.pipe(map((segments) => segments.join("")));
     this.userRoles$ = this.store.select(getAllUSerRoles);
     // this.store.dispatch(loadSessionDetails());
     this.store.dispatch(loadSpecimenSources());
@@ -103,29 +104,22 @@ export class LaboratoryComponent implements OnInit {
       ...this.parameters,
       startDate: formatDateToYYMMDD(
         new Date(
-          Number(today.split('-')[0]),
-          Number(today.split('-')[1]) - 1,
-          Number(today.split('-')[2]) - 1
+          Number(today.split("-")[0]),
+          Number(today.split("-")[1]) - 1,
+          Number(today.split("-")[2]) - this.datesRangeDifference
         )
       ),
       endDate: formatDateToYYMMDD(
         new Date(
-          Number(today.split('-')[0]),
-          Number(today.split('-')[1]) - 1,
-          Number(today.split('-')[2])
+          Number(today.split("-")[0]),
+          Number(today.split("-")[1]) - 1,
+          Number(today.split("-")[2])
         )
       ),
     };
 
     this.startDate = this.parameters?.startDate;
     this.endDate = this.parameters?.endDate;
-
-    // this.store.dispatch(
-    //   loadActiveVisitsWithLabOrders({
-    //     startDate: this.parameters?.startDate,
-    //     endDate: this.parameters?.endDate,
-    //   })
-    // );
 
     this.store.dispatch(
       loadLabConfigurations({ periodParameters: this.parameters })
@@ -146,13 +140,6 @@ export class LaboratoryComponent implements OnInit {
 
   enableDate(dateRange) {
     this.showDate = true;
-
-    // console.log(dateRange);
-    // console.log('parameters', this.parameters);
-    // console.log('date :: ', new Date(this.parameters?.startDate));
-
-    // console.log('path form', this.route.snapshot.pathFromRoot);
-
     let endDate =
       dateRange == 6
         ? formatDateToYYMMDD(new Date())
@@ -162,16 +149,16 @@ export class LaboratoryComponent implements OnInit {
       ...this.parameters,
       startDate: formatDateToYYMMDD(
         new Date(
-          Number(endDate?.split('-')[0]),
-          Number(endDate?.split('-')[1]) - 1,
-          Number(endDate?.split('-')[2]) - dateRange
+          Number(endDate?.split("-")[0]),
+          Number(endDate?.split("-")[1]) - 1,
+          Number(endDate?.split("-")[2]) - dateRange
         )
       ),
       endDate: formatDateToYYMMDD(
         new Date(
-          Number(endDate?.split('-')[0]),
-          Number(endDate?.split('-')[1]) - 1,
-          Number(endDate?.split('-')[2])
+          Number(endDate?.split("-")[0]),
+          Number(endDate?.split("-")[1]) - 1,
+          Number(endDate?.split("-")[2])
         )
       ),
     };
@@ -179,23 +166,10 @@ export class LaboratoryComponent implements OnInit {
     this.startDate = this.parameters?.startDate;
     this.endDate = this.parameters?.endDate;
 
-    if (dateRange == 6) {
-      // this.store.dispatch(
-      //   loadActiveVisitsWithLabOrders({
-      //     startDate: this.parameters?.startDate,
-      //     endDate: this.parameters?.endDate,
-      //   })
-      // );
-    }
-
     this.store.dispatch(setVisitsParameters({ parameters: this.parameters }));
-
-    // this.store.dispatch()
   }
 
   onDateChange(reload?: boolean) {
-    // console.log('the date', reload, ' --- ', this.endDate);
-
     if (reload && this.endDate) {
       this.store.dispatch(clearLoadedLabOrders());
 
@@ -219,7 +193,7 @@ export class LaboratoryComponent implements OnInit {
 
       // this.store.dispatch(loadActiveVisits({ parameters: this.parameters }));
 
-      if (this.currentSubModule == 'collection') {
+      if (this.currentSubModule == "collection") {
         // console.log('here');
         // this.store.dispatch(
         //   loadActiveVisitsWithLabOrders({
