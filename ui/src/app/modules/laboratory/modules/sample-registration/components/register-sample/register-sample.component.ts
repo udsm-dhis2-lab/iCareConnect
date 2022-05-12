79e811cbf6a741f3f462a754837ba26a88526acc
josephatJ <josephatjulius24@gmail.com>
2022-05-12 18:34:58 +0300

    Add support to capture more details for a person

--- ui/src/app/modules/laboratory/modules/sample-registration/components/register-sample/register-sample.component.ts
+++ ui/src/app/modules/laboratory/modules/sample-registration/components/register-sample/register-sample.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { MatRadioChange } from "@angular/material/radio";
 import { Observable } from "rxjs";
+import { SystemSettingsService } from "src/app/core/services/system-settings.service";
 import { LabSampleModel } from "src/app/modules/laboratory/resources/models";
 import { SamplesService } from "src/app/shared/services/samples.service";
 
@@ -13,10 +14,23 @@ export class RegisterSampleComponent implements OnInit {
   registrationCategory: string = "single";
 
   labSamples$: Observable<LabSampleModel[]>;
-  constructor(private samplesService: SamplesService) {}
+  mrnGeneratorSourceUuid$: Observable<string>;
+  preferredPersonIdentifier$: Observable<string>;
+  constructor(
+    private samplesService: SamplesService,
+    private systemSettingsService: SystemSettingsService
+  ) {}
 
   ngOnInit(): void {
     this.labSamples$ = this.samplesService.getCollectedSamples();
+    this.mrnGeneratorSourceUuid$ =
+      this.systemSettingsService.getSystemSettingsByKey(
+        "iCare.generateMRN.source"
+      );
+    this.preferredPersonIdentifier$ =
+      this.systemSettingsService.getSystemSettingsByKey(
+        "iCare.preferredPersonIdentifier"
+      );
   }
 
   getSelection(event: MatRadioChange): void {
--- ui/src/app/modules/laboratory/modules/sample-registration/components/single-registration/single-registration.component.ts
+++ ui/src/app/modules/laboratory/modules/sample-registration/components/single-registration/single-registration.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable, zip } from "rxjs";
+import { Location } from "src/app/core/models";
+import { LocationService } from "src/app/core/services";
+import { IdentifiersService } from "src/app/core/services/identifiers.service";
 import { LabSampleModel } from "src/app/modules/laboratory/resources/models";
 import { LabTestsService } from "src/app/modules/laboratory/resources/services/lab-tests.service";
+import { RegistrationService } from "src/app/modules/registration/services/registration.services";
 import { Dropdown } from "src/app/shared/modules/form/models/dropdown.model";
 import { FormValue } from "src/app/shared/modules/form/models/form-value.model";
 import { SamplesService } from "src/app/shared/services/samples.service";
@@ -13,14 +17,25 @@ import { SamplesService } from "src/app/shared/services/samples.service";
 })
 export class SingleRegistrationComponent implements OnInit {
   labSampleLabel$: Observable<string>;
+  @Input() mrnGeneratorSourceUuid: string;
+  @Input() preferredPersonIdentifier: string;
 
   departmentField: any = {};
   formData: any = {};
   testsUnderDepartment$: Observable<any[]>;
+
+  currentLocation: Location;
+  patientPayload: any;
+  personDetailsData: any;
   constructor(
     private samplesService: SamplesService,
-    private labTestsService: LabTestsService
-  ) {}
+    private labTestsService: LabTestsService,
+    private locationService: LocationService,
+    private registrationService: RegistrationService,
+    private identifierService: IdentifiersService
+  ) {
+    this.currentLocation = JSON.parse(localStorage.getItem("currentLocation"));
+  }
 
   ngOnInit(): void {
     this.labSampleLabel$ = this.samplesService.getSampleLabel();
@@ -36,12 +51,83 @@ export class SingleRegistrationComponent implements OnInit {
   }
 
   onFormUpdate(formValues: FormValue, itemKey?: string): void {
-    console.log(formValues.getValues());
     this.formData = { ...this.formData, ...formValues.getValues() };
     if (itemKey === "department") {
       this.testsUnderDepartment$ = this.labTestsService.getLabTestsByDepartment(
         this.formData["department"]?.value
       );
     }
+  }
+
+  onGetPersonDetails(personDetails: any): void {
+    this.personDetailsData = personDetails;
+  }
+
+  onSave(event: Event): void {
+    event.stopPropagation();
+    zip(
+      this.registrationService.getPatientIdentifierTypes(),
+      this.locationService.getFacilityCode(),
+      this.registrationService.getAutoFilledPatientIdentifierType()
+    ).subscribe((results) => {
+      if (results) {
+        const patientIdentifierTypes = results[0];
+        this.identifierService
+          .generateIds({
+            generateIdentifiers: true,
+            sourceUuid: this.mrnGeneratorSourceUuid,
+            numberToGenerate: 1,
+          })
+          .subscribe((identifierResponse) => {
+            if (identifierResponse) {
+              /**
+            1. Create user
+            2. Create visit (Orders should be added in)
+            3. Create sample
+            */
+
+              this.patientPayload = {
+                person: {
+                  names: [
+                    {
+                      givenName: this.personDetailsData?.firstName,
+                      familyName: this.personDetailsData?.lastName,
+                    },
+                  ],
+                  gender: this.personDetailsData?.gender,
+                  dob: null,
+                  addresses: [],
+                  attributes: [],
+                },
+                identifiers: (patientIdentifierTypes || [])
+                  .map((personIdentifierType) => {
+                    if (
+                      personIdentifierType.id === this.preferredPersonIdentifier
+                    ) {
+                      return {
+                        identifier:
+                          this.personDetailsData[personIdentifierType.id],
+                        identifierType: personIdentifierType.id,
+                        location: this.currentLocation.uuid,
+                        preferred: true,
+                      };
+                    } else {
+                      return {
+                        identifier:
+                          this.personDetailsData[personIdentifierType.id],
+                        identifierType: personIdentifierType.id,
+                        location: this.currentLocation.uuid,
+                        preferred: false,
+                      };
+                    }
+                  })
+                  .filter((patientIdentifier) => patientIdentifier?.identifier),
+              };
+
+              console.log(this.patientPayload);
+            }
+          });
+      }
+    });
   }
 }
