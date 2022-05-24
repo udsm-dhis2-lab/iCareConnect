a28c1c272334dd0995f5ab53538ecccce505e9d0
josephatJ <josephatjulius24@gmail.com>
2022-05-25 01:49:05 +0300

    Fix clinical notes issue

--- ui/src/app/shared/resources/diagnosis/services/diagnosis.service.ts
+++ ui/src/app/shared/resources/diagnosis/services/diagnosis.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Api } from "../../openmrs";
-import { Observable, from } from "rxjs";
+import { Observable, from, of } from "rxjs";
 
 import { omit } from "lodash";
+import { catchError, map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -13,16 +14,25 @@ export class DiagnosisService {
   // concept?code=Diagnosis+Concept+Set&source=org.openmrs.module.emrapi&v=custom:(uuid,name,setMembers)
   addDiagnosis(data: any, currentDiagnosisUuid?: string): Observable<any> {
     if (!currentDiagnosisUuid) {
-      return from(this.api.patientdiagnoses.createDiagnosis(data));
+      return from(this.api.patientdiagnoses.createDiagnosis(data)).pipe(
+        map((response) => response),
+        catchError((error) => of(error))
+      );
     } else {
       data = omit(data, "patient");
       return from(
         this.api.patientdiagnoses.updateDiagnosis(currentDiagnosisUuid, data)
+      ).pipe(
+        map((response) => response),
+        catchError((error) => of(error))
       );
     }
   }
 
   deleteDiagnosis(uuid): Observable<any> {
-    return from(this.api.patientdiagnoses.deleteDiagnosis(uuid));
+    return from(this.api.patientdiagnoses.deleteDiagnosis(uuid)).pipe(
+      map((response) => response),
+      catchError((error) => of(error))
+    );
   }
 }
