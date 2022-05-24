fb1e2c50ae0abb53a006064027366e246a6733cd
josephatJ <josephatjulius24@gmail.com>
2022-05-25 01:49:05 +0300

    Fix clinical notes issue

--- ui/src/app/modules/laboratory/modules/sample-acceptance-and-results/components/index.ts
+++ ui/src/app/modules/laboratory/modules/sample-acceptance-and-results/components/index.ts
@@ -1,30 +1,31 @@
-import { PatientsByVisitsListComponent } from './patients-by-visits-list/patients-by-visits-list.component';
-import { PatientOrdersComponent } from './patient-orders/patient-orders.component';
-import { SampleArrivalDashboardComponent } from './sample-arrival-dashboard/sample-arrival-dashboard.component';
-import { SamplesCollectedComponent } from './samples-collected/samples-collected.component';
-import { SamplesToAcceptComponent } from './samples-to-accept/samples-to-accept.component';
-import { TestAllocationDashboardComponent } from './test-allocation-dashboard/test-allocation-dashboard.component';
-import { ResultsApprovalDashboardComponent } from './results-approval-dashboard/results-approval-dashboard.component';
-import { ResultsApprovalComponent } from './results-approval/results-approval.component';
-import { SummarySampleCollectedComponent } from './summary-sample-collected/summary-sample-collected.component';
-import { SummarySamplePendingComponent } from './summary-sample-pending/summary-sample-pending.component';
-import { SummarySamplesRejectedComponent } from './summary-samples-rejected/summary-samples-rejected.component';
-import { SummarySamplesWithResultsComponent } from './summary-samples-with-results/summary-samples-with-results.component';
-import { SummarySampleStatusDashboardComponent } from './summary-sample-status-dashboard/summary-sample-status-dashboard.component';
-import { SummarySamplesAcceptedComponent } from './summary-samples-accepted/summary-samples-accepted.component';
-import { SummarySamplesToCollectComponent } from './summary-samples-to-collect/summary-samples-to-collect.component';
-import { SampleTrackingDashboardComponent } from './sample-tracking-dashboard/sample-tracking-dashboard.component';
-import { SampleTrackingComponent } from './sample-tracking/sample-tracking.component';
-import { RejectionReasonComponent } from './rejection-reason/rejection-reason.component';
-import { ResultsFeedingModalComponent } from './results-feeding-modal/results-feeding-modal.component';
-import { SampleTrackingModalComponent } from './sample-tracking-modal/sample-tracking-modal.component';
-import { PrintResultsModalComponent } from './print-results-modal/print-results-modal.component';
-import { ResultsToPrintComponent } from './results-to-print/results-to-print.component';
-import { SampleStatusSummaryForHomeComponent } from './sample-status-summary-for-home/sample-status-summary-for-home.component';
-import { BarCodeModalComponent } from './bar-code-modal/bar-code-modal.component';
-import { UploadFileComponent } from './upload-file/upload-file.component';
-import { SampleAcceptanceComponent } from './sample-acceptance/sample-acceptance.component';
-import { RejectAnswerModalComponent } from './reject-answer-modal/reject-answer-modal.component';
+import { PatientsByVisitsListComponent } from "./patients-by-visits-list/patients-by-visits-list.component";
+import { PatientOrdersComponent } from "./patient-orders/patient-orders.component";
+import { SampleArrivalDashboardComponent } from "./sample-arrival-dashboard/sample-arrival-dashboard.component";
+import { SamplesCollectedComponent } from "./samples-collected/samples-collected.component";
+import { SamplesToAcceptComponent } from "./samples-to-accept/samples-to-accept.component";
+import { TestAllocationDashboardComponent } from "./test-allocation-dashboard/test-allocation-dashboard.component";
+import { ResultsApprovalDashboardComponent } from "./results-approval-dashboard/results-approval-dashboard.component";
+import { ResultsApprovalComponent } from "./results-approval/results-approval.component";
+import { SummarySampleCollectedComponent } from "./summary-sample-collected/summary-sample-collected.component";
+import { SummarySamplePendingComponent } from "./summary-sample-pending/summary-sample-pending.component";
+import { SummarySamplesRejectedComponent } from "./summary-samples-rejected/summary-samples-rejected.component";
+import { SummarySamplesWithResultsComponent } from "./summary-samples-with-results/summary-samples-with-results.component";
+import { SummarySampleStatusDashboardComponent } from "./summary-sample-status-dashboard/summary-sample-status-dashboard.component";
+import { SummarySamplesAcceptedComponent } from "./summary-samples-accepted/summary-samples-accepted.component";
+import { SummarySamplesToCollectComponent } from "./summary-samples-to-collect/summary-samples-to-collect.component";
+import { SampleTrackingDashboardComponent } from "./sample-tracking-dashboard/sample-tracking-dashboard.component";
+import { SampleTrackingComponent } from "./sample-tracking/sample-tracking.component";
+import { RejectionReasonComponent } from "./rejection-reason/rejection-reason.component";
+import { ResultsFeedingModalComponent } from "./results-feeding-modal/results-feeding-modal.component";
+import { SampleTrackingModalComponent } from "./sample-tracking-modal/sample-tracking-modal.component";
+import { PrintResultsModalComponent } from "./print-results-modal/print-results-modal.component";
+import { ResultsToPrintComponent } from "./results-to-print/results-to-print.component";
+import { SampleStatusSummaryForHomeComponent } from "./sample-status-summary-for-home/sample-status-summary-for-home.component";
+import { BarCodeModalComponent } from "./bar-code-modal/bar-code-modal.component";
+import { UploadFileComponent } from "./upload-file/upload-file.component";
+import { SampleAcceptanceComponent } from "./sample-acceptance/sample-acceptance.component";
+import { RejectAnswerModalComponent } from "./reject-answer-modal/reject-answer-modal.component";
+import { LabClinicalNotesSummaryComponent } from "./lab-clinical-notes-summary/lab-clinical-notes-summary.component";
 
 export const components: any[] = [
   PatientsByVisitsListComponent,
@@ -54,4 +55,5 @@ export const components: any[] = [
   UploadFileComponent,
   SampleAcceptanceComponent,
   RejectAnswerModalComponent,
+  LabClinicalNotesSummaryComponent,
 ];
