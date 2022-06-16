7e28538679c8ff265dc2cc830c829ba84ea3ee57
josephatJ <josephatjulius24@gmail.com>
2022-06-16 10:59:36 +0300

    Add support to capture specimen setmembers as required tests

--- ui/src/app/modules/laboratory/resources/services/lab-tests.service.ts
+++ ui/src/app/modules/laboratory/resources/services/lab-tests.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { from, Observable, of, zip } from "rxjs";
 import { catchError, map } from "rxjs/operators";
 import { OpenmrsHttpClientService } from "src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service";
-import { Api } from "src/app/shared/resources/openmrs";
+import { Api, ConceptGet } from "src/app/shared/resources/openmrs";
 
 @Injectable({
   providedIn: "root",
@@ -12,6 +12,19 @@ export class LabTestsService {
     private api: Api,
     private openMRSHttpClient: OpenmrsHttpClientService
   ) {}
+
+  getSetMembersByConceptUuid(uuid: string): Observable<ConceptGet[]> {
+    return this.openMRSHttpClient
+      .get(
+        `concept/${uuid}?v=custom:(uuid,display,setMembers:(uuid,display,name))`
+      )
+      .pipe(
+        map((response) => {
+          return response?.setMembers;
+        }),
+        catchError((error) => of(error))
+      );
+  }
 
   getLabTestsByDepartment(uuid: string): Observable<any> {
     return this.openMRSHttpClient
