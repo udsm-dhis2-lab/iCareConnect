76fb0a49d9c2b13e60bcde0fad0cfd6c746fac91
josephatJ <josephatjulius24@gmail.com>
2022-06-24 21:04:21 +0300

    Add support to search item on multiple items selection

--- ui/src/app/shared/components/multiple-items-selection/multiple-items-selection.component.ts
+++ ui/src/app/shared/components/multiple-items-selection/multiple-items-selection.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { uniqBy, orderBy } from "lodash";
+import { Observable, of } from "rxjs";
+import { ConceptsService } from "../../resources/concepts/services/concepts.service";
 
 @Component({
   selector: "app-multiple-items-selection",
@@ -9,23 +11,38 @@ import { uniqBy, orderBy } from "lodash";
 export class MultipleItemsSelectionComponent implements OnInit {
   @Input() items: any[];
   @Input() selectedItems: any[];
+  @Input() itemType: string;
+  @Input() standardSearchTerm: string;
   currentSelectedItems: any[] = [];
   @Output() getSelectedItems: EventEmitter<any[]> = new EventEmitter<any[]>();
-  constructor() {}
+  items$: Observable<any[]>;
+  page: number = 1;
+  pageSize: number = 10;
+  constructor(private conceptService: ConceptsService) {}
 
   ngOnInit(): void {
     this.currentSelectedItems = this.selectedItems;
-    this.items = this.items.filter(
-      (itemFromAll) =>
-        (
-          this.currentSelectedItems?.filter(
-            (item) => item?.uuid === itemFromAll?.uuid
-          ) || []
-        )?.length === 0
-    );
+    if (this.itemType && this.standardSearchTerm) {
+      this.items$ = this.conceptService.getConceptsByParameters({
+        searchingText: this.standardSearchTerm,
+        page: this.page,
+        pageSize: this.pageSize,
+      });
+    } else {
+      this.items$ = of(
+        this.items.filter(
+          (itemFromAll) =>
+            (
+              this.currentSelectedItems?.filter(
+                (item) => item?.uuid === itemFromAll?.uuid
+              ) || []
+            )?.length === 0
+        )
+      );
+    }
   }
 
-  getSelectedItem(event: Event, item: any): void {
+  getSelectedItem(event: Event, item: any, items: any[]): void {
     event.stopPropagation();
     this.currentSelectedItems = uniqBy([...this.selectedItems, item]);
     this.items = this.items.filter(
@@ -37,10 +54,11 @@ export class MultipleItemsSelectionComponent implements OnInit {
         )?.length === 0
     );
     this.items = orderBy(this.items, ["display"], ["asc"]);
+    this.items$ = of(items);
     this.getSelectedItems.emit(this.currentSelectedItems);
   }
 
-  removeSelectedItem(event: Event, item: any): void {
+  removeSelectedItem(event: Event, item: any, items: any[]): void {
     event.stopPropagation();
     this.currentSelectedItems = uniqBy(
       this.currentSelectedItems.filter(
@@ -48,10 +66,29 @@ export class MultipleItemsSelectionComponent implements OnInit {
       )
     );
     this.items = orderBy([...this.items, item], ["display"], ["asc"]);
+    this.items$ = of(items);
     this.getSelectedItems.emit(this.currentSelectedItems);
   }
 
-  searchItem(event): void {
-    console.log(event);
+  searchItem(event: KeyboardEvent): void {
+    this.page = 1;
+    const searchingText = (event.target as HTMLInputElement).value;
+    if (searchingText && this.itemType === "concept") {
+      this.items$ = this.conceptService.getConceptsByParameters({
+        searchingText,
+        pageSize: this.pageSize,
+        page: this.page,
+      });
+    }
+  }
+
+  getItems(event: Event, actionType: string, itemType: string): void {
+    event.stopPropagation();
+    this.page = actionType === "prev" ? this.page - 1 : this.page + 1;
+    this.items$ = this.conceptService.getConceptsByParameters({
+      searchingText: this.standardSearchTerm,
+      pageSize: this.pageSize,
+      page: this.page,
+    });
   }
 }
