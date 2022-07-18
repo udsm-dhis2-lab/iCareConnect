92ff7aa7456a8564d46eebddd4f2705e646f85f5
josephatJ <josephatjulius24@gmail.com>
2022-07-18 19:01:53 +0300

    import regexp packages

--- omods/core/api/src/main/java/org/openmrs/module/icare/core/impl/ICareServiceImpl.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/core/impl/ICareServiceImpl.java
@@ -45,7 +45,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class ICareServiceImpl extends BaseOpenmrsService implements ICareService {
 	
 	ICareDao dao;
