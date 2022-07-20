2d8d138cadcaf664f4d4f40f5fcd1c216c722806
josephatJ <josephatjulius24@gmail.com>
2022-07-20 18:46:14 +0300

    change search term type

--- ui/src/app/shared/components/multiple-items-selection/multiple-items-selection.component.ts
+++ ui/src/app/shared/components/multiple-items-selection/multiple-items-selection.component.ts
@@ -27,6 +27,7 @@ export class MultipleItemsSelectionComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    console.log("standardSearchTerm", this.standardSearchTerm);
     this.currentSelectedItems = this.selectedItems;
     if (
       this.itemType &&
