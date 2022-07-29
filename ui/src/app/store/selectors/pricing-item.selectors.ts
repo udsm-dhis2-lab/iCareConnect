5bda0dab3e2d0cfe1857046aae173ecb3f750027
josephatJ <josephatjulius24@gmail.com>
2022-07-29 09:45:26 +0300

    Fix duplication in pricing list

--- ui/src/app/store/selectors/pricing-item.selectors.ts
+++ ui/src/app/store/selectors/pricing-item.selectors.ts
@@ -1,19 +1,17 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { getPaymentTypeLoadingState } from 'src/app/store/selectors/payment-type.selectors';
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { getPaymentTypeLoadingState } from "src/app/store/selectors/payment-type.selectors";
 import {
   pricingItemAdapter,
   PricingItemState,
-} from '../states/pricing-item.state';
+} from "../states/pricing-item.state";
 
-import { getItemPriceLoadingStatus } from './item-price.selectors';
+import { getItemPriceLoadingStatus } from "./item-price.selectors";
 
-const getPricingItemState = createFeatureSelector<PricingItemState>(
-  'pricingItem'
-);
+const getPricingItemState =
+  createFeatureSelector<PricingItemState>("pricingItem");
 
-export const {
-  selectAll: getAllPricingItems,
-} = pricingItemAdapter.getSelectors(getPricingItemState);
+export const { selectAll: getAllPricingItems } =
+  pricingItemAdapter.getSelectors(getPricingItemState);
 
 export const getPricingItemLoadingState = createSelector(
   getPricingItemState,
