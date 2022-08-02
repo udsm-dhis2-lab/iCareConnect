e2e34d921aa0e54ed5684e6a40fc6537e62e9798
josephatJ <josephatjulius24@gmail.com>
2022-08-02 18:16:21 +0300

    Add support to list locations grouped by location tags

--- ui/src/app/shared/components/shared-location-settings/shared-location-settings.component.ts
+++ ui/src/app/shared/components/shared-location-settings/shared-location-settings.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { Observable } from "rxjs";
 import { LocationService } from "src/app/core/services";
-import { SystemSettingsService } from "src/app/core/services/system-settings.service";
-import { LocationtagGetFull } from "../../resources/openmrs";
+import { LocationGetFull, LocationtagGetFull } from "../../resources/openmrs";
+import { ManageLocationModalComponent } from "../manage-location-modal/manage-location-modal.component";
 
 @Component({
   selector: "app-shared-location-settings",
@@ -10,17 +11,44 @@ import { LocationtagGetFull } from "../../resources/openmrs";
   styleUrls: ["./shared-location-settings.component.scss"],
 })
 export class SharedLocationSettingsComponent implements OnInit {
-  @Input() key: string;
-  locationTags$: Observable<LocationtagGetFull[]>;
-  locationTagsUuids$: Observable<any[]>;
+  @Input() locationTag: LocationtagGetFull;
+  @Input() locationTags: LocationtagGetFull[];
+  locationsByTag$: Observable<LocationGetFull[]>;
+  page: number = 1;
+  pageSize: number = 10;
   constructor(
     private locationService: LocationService,
-    private systemSettingService: SystemSettingsService
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
-    this.locationTags$ = this.locationService.getLocationTags();
-    this.locationTagsUuids$ =
-      this.systemSettingService.getSystemSettingsDetailsByKey(this.key);
+    this.getLocations();
+  }
+
+  getLocations(): void {
+    this.locationsByTag$ = this.locationService.getLocationsByTagName(
+      this.locationTag?.display,
+      {
+        limit: this.pageSize,
+        startIndex: (this.page - 1) * this.pageSize,
+      }
+    );
+  }
+
+  openModal(event: Event, locationTag: any): void {
+    event.stopPropagation();
+    this.dialog.open(ManageLocationModalComponent, {
+      width: "50%",
+      data: {
+        locationTag,
+        locationTags: this.locationTags,
+      },
+    });
+  }
+
+  getItems(event: Event, actionType: string): void {
+    event.stopPropagation();
+    this.page = actionType === "next" ? this.page + 1 : this.page - 1;
+    this.getLocations();
   }
 }
--- ui/src/app/shared/components/shared-location-type-selection/shared-location-type-selection.component.ts
+++ ui/src/app/shared/components/shared-location-type-selection/shared-location-type-selection.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { LocationtagGetFull } from "../../resources/openmrs";
 
 @Component({
-  selector: 'app-shared-location-type-selection',
-  templateUrl: './shared-location-type-selection.component.html',
-  styleUrls: ['./shared-location-type-selection.component.scss']
+  selector: "app-shared-location-type-selection",
+  templateUrl: "./shared-location-type-selection.component.html",
+  styleUrls: ["./shared-location-type-selection.component.scss"],
 })
 export class SharedLocationTypeSelectionComponent implements OnInit {
+  @Input() locationTags: LocationtagGetFull[];
+  @Input() allowedLocationTagsReferences: any[];
 
-  constructor() { }
+  headers: any[];
+  constructor() {}
 
   ngOnInit(): void {
+    this.headers = this.allowedLocationTagsReferences.map((allowedTag) => {
+      return {
+        ...allowedTag,
+        ...(this.locationTags.filter(
+          (tag) => tag?.uuid === allowedTag?.value
+        ) || [])[0],
+      };
+    });
   }
-
 }
