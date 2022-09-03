96c54924140ace999b3076a90aab45f4805f8c6c
Vincent Minde <vincentminde@gmail.com>
2022-09-03 19:21:57 +0300

    Improved compilation performance

--- docker/build-local.sh
+++ docker/build-local.sh
@@ -1,10 +1,11 @@
 branch=$(git branch | grep \* | cut -d ' ' -f2)
 cd ui
-docker run -w="/app" -v "$(pwd)":/app node:14.18.3-alpine3.14 npm install
-docker run -w="/app" -v "$(pwd)":/app node:14.18.3-alpine3.14 npm run build:prod
-cd ../docs
-docker run -w="/app" -v "$(pwd)":/app node:16.14 npm install
-docker run -w="/app" -v "$(pwd)":/app node:16.14 npm run build
+
+docker run -w="/app" -v "$(pwd)":/app udsmdhis2/icare-ui-compiler mv /node_modules .
+docker run -w="/app" -v "$(pwd)":/app udsmdhis2/icare-ui-compiler npm run build:prod
+#cd ../docs
+#docker run -w="/app" -v "$(pwd)":/app node:16.14 npm install
+#docker run -w="/app" -v "$(pwd)":/app node:16.14 npm run build
 cd ..
 docker run --rm -v maven-repo:/root/.m2 -v $(pwd)/omods/core:/usr/src/omod -w /usr/src/omod maven:3.6.3-openjdk-11-slim mvn clean package -DskipTests
 version=$(cat version)
