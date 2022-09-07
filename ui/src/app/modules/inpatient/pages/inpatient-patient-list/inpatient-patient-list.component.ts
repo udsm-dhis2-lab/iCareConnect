d6ce0bc22fa4a8da4e24f15c0f5bcfed66feb9c1
josephatJ <josephatjulius24@gmail.com>
2022-09-07 18:47:58 +0300

    Fix IPD module issue

--- ui/src/app/modules/inpatient/pages/inpatient-patient-list/inpatient-patient-list.component.ts
+++ ui/src/app/modules/inpatient/pages/inpatient-patient-list/inpatient-patient-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { AppState } from 'src/app/store/reducers';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { loadLocationById } from "src/app/store/actions";
+import { AppState } from "src/app/store/reducers";
 import {
   getCurrentLocation,
   getSettingCurrentLocationStatus,
-} from 'src/app/store/selectors';
+} from "src/app/store/selectors";
 
 @Component({
-  selector: 'app-inpatient-patient-list',
-  templateUrl: './inpatient-patient-list.component.html',
-  styleUrls: ['./inpatient-patient-list.component.scss'],
+  selector: "app-inpatient-patient-list",
+  templateUrl: "./inpatient-patient-list.component.html",
+  styleUrls: ["./inpatient-patient-list.component.scss"],
 })
 export class InpatientPatientListComponent implements OnInit {
   currentLocation$: Observable<Location>;
@@ -18,6 +19,11 @@ export class InpatientPatientListComponent implements OnInit {
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.dispatch(
+      loadLocationById({
+        locationUuid: JSON.parse(localStorage.getItem("currentLocation"))?.uuid,
+      })
+    );
     this.currentLocation$ = this.store.select(getCurrentLocation);
     this.settingCurrentLocationStatus$ = this.store.select(
       getSettingCurrentLocationStatus
