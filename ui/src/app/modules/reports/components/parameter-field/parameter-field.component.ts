245a52cb93cf41a61c75a23c181bf9ab3e63cfc7
Marco Masembo <marcomasembo1@gmail.com>
2022-09-14 16:49:45 +0300

    fixing bug on report module not accepting other inputs

--- ui/src/app/modules/reports/components/parameter-field/parameter-field.component.ts
+++ ui/src/app/modules/reports/components/parameter-field/parameter-field.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { getSanitizedParamValue } from '../../helpers/get-sanitized-param-value.helper';
-import { ReportParam } from '../../models/report-params.model';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { getSanitizedParamValue } from "../../helpers/get-sanitized-param-value.helper";
+import { ReportParam } from "../../models/report-params.model";
 
 @Component({
-  selector: 'app-parameter-field',
-  templateUrl: './parameter-field.component.html',
-  styleUrls: ['./parameter-field.component.scss'],
+  selector: "app-parameter-field",
+  templateUrl: "./parameter-field.component.html",
+  styleUrls: ["./parameter-field.component.scss"],
 })
 export class ParameterFieldComponent implements OnInit {
   @Input() field: ReportParam;
@@ -31,28 +31,29 @@ export class ParameterFieldComponent implements OnInit {
 
   onParamChange(e, paramId, selectedDateTime?) {
     let value;
-    if (paramId.toLowerCase() == 'startdate') {
+    if (paramId.toLowerCase() == "startdate") {
       value = `${e?.value?.getFullYear()}-${
         e?.value?.getMonth() + 1
       }-${e?.value?.getDate()}T${
-        selectedDateTime ? new Date(selectedDateTime).getHours() : '00'
+        selectedDateTime ? new Date(selectedDateTime).getHours() : "00"
       }:${
-        selectedDateTime ? new Date(selectedDateTime).getMinutes() : '00'
+        selectedDateTime ? new Date(selectedDateTime).getMinutes() : "00"
       }:00`;
     }
 
-    if (paramId.toLowerCase() == 'enddate') {
+    if (paramId.toLowerCase() == "enddate") {
       value = `${e?.value?.getFullYear()}-${
         e?.value?.getMonth() + 1
       }-${e?.value?.getDate()}T${
-        selectedDateTime ? new Date(selectedDateTime).getHours() : '23'
+        selectedDateTime ? new Date(selectedDateTime).getHours() : "23"
       }:${
-        selectedDateTime ? new Date(selectedDateTime).getMinutes() : '59'
+        selectedDateTime ? new Date(selectedDateTime).getMinutes() : "59"
       }:59`;
     }
     this.parameterUpdate.emit({
       // [paramId]: getSanitizedParamValue(e.value || e.checked, this.field?.type),
-      [paramId]: value || e?.value || e?.checked || this.fieldValue,
+      [paramId]:
+        value || e?.target?.value || e?.value || e?.checked || this.fieldValue,
     });
   }
 }
