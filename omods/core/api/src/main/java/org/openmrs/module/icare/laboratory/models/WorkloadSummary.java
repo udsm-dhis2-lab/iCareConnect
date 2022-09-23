395497eedf6907d7c858e348d49edd05c3cc5356
Dkiba <kibahiladennis@gmail.com>
2022-09-23 10:57:23 +0300

    confirming the created columns

--- omods/core/api/src/main/java/org/openmrs/module/icare/laboratory/models/WorkloadSummary.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/laboratory/models/WorkloadSummary.java
@@ -4,15 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WorkloadSummary {
-
-    private long samplesWithNoResults;
-    private long samplesWithRejectedResults;
-    private long samplesWithResults;
-
-    private long samplesAuthorized;
-
-
-    public Map<String,Object> toMap(){
+	
+	private long samplesWithNoResults;
+	
+	private long samplesWithRejectedResults;
+	
+	private long samplesWithResults;
+	
+	private long samplesAuthorized;
+	
+	public Map<String,Object> toMap(){
         Map<String,Object> result = new HashMap<>();
         result.put("samplesWithNoResults",this.samplesWithNoResults);
         result.put("samplesWithRejectedResults",this.samplesWithRejectedResults);
@@ -21,38 +22,36 @@ public class WorkloadSummary {
 
         return  result;
     }
-
-    public long getSamplesWithNoResults() {
-        return samplesWithNoResults;
-    }
-
-    public void setSamplesWithNoResults(long samplesWithNoResults) {
-        this.samplesWithNoResults = samplesWithNoResults;
-    }
-
-
-
-    public long getSamplesWithRejectedResults() {
-        return samplesWithRejectedResults;
-    }
-
-    public void setSamplesWithRejectedResults(long samplesWithRejectedResults) {
-        this.samplesWithRejectedResults = samplesWithRejectedResults;
-    }
-
-    public long getSamplesAuthorized() {
-        return samplesAuthorized;
-    }
-
-    public void setSamplesAuthorized(long samplesAuthorized) {
-        this.samplesAuthorized = samplesAuthorized;
-    }
-
-    public long getSamplesWithResults() {
-        return samplesWithResults;
-    }
-
-    public void setSamplesWithResults(long samplesWithResults) {
-        this.samplesWithResults = samplesWithResults;
-    }
+	
+	public long getSamplesWithNoResults() {
+		return samplesWithNoResults;
+	}
+	
+	public void setSamplesWithNoResults(long samplesWithNoResults) {
+		this.samplesWithNoResults = samplesWithNoResults;
+	}
+	
+	public long getSamplesWithRejectedResults() {
+		return samplesWithRejectedResults;
+	}
+	
+	public void setSamplesWithRejectedResults(long samplesWithRejectedResults) {
+		this.samplesWithRejectedResults = samplesWithRejectedResults;
+	}
+	
+	public long getSamplesAuthorized() {
+		return samplesAuthorized;
+	}
+	
+	public void setSamplesAuthorized(long samplesAuthorized) {
+		this.samplesAuthorized = samplesAuthorized;
+	}
+	
+	public long getSamplesWithResults() {
+		return samplesWithResults;
+	}
+	
+	public void setSamplesWithResults(long samplesWithResults) {
+		this.samplesWithResults = samplesWithResults;
+	}
 }
