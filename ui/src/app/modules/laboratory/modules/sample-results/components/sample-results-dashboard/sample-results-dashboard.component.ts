414888d10bc9de00a7b022f379f73e3da27824d8
jonas1015 <jonasgsmart1015@gmail.com>
2022-09-23 12:55:04 +0300

    Added a page to allow user to release or restrict external usage of results

--- ui/src/app/modules/laboratory/modules/sample-results/components/sample-results-dashboard/sample-results-dashboard.component.ts
+++ ui/src/app/modules/laboratory/modules/sample-results/components/sample-results-dashboard/sample-results-dashboard.component.ts
@@ -1,9 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/app/store/reducers';
-import { getCurrentUserPrivileges } from 'src/app/store/selectors/current-user.selectors';
-import { getSampleTypesLoadedState, getVisitsLoadedState, getAllSampleTypes, getAllVisits, getLabConfigurations, getLabTestsContainers, getLabSampleContainers, getCodedSampleRejectionReassons, getLabDepartments, getLabOrdersWithFirstSignOff, getLabOrdersWithSecondSignOff, getAcceptedLabOrders, getLabOrdersWithIntermediateResults, getRejectedLabOrders, getSampleIdentifierDetails, getAllLabOrders, getCollectedLabOrders, getLabOrdersLoadedState } from 'src/app/store/selectors';
+import { Component, Input, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { filter, map, tap } from "rxjs/operators";
+import { LISConfigurationsModel } from "src/app/modules/laboratory/resources/models/lis-configurations.model";
+import { DeleteConfirmationComponent } from "src/app/shared/components/delete-confirmation/delete-confirmation.component";
+import {
+  addLabDepartments,
+  loadLabSamplesByCollectionDates,
+} from "src/app/store/actions";
+import { AppState } from "src/app/store/reducers";
+import {
+  getCodedSampleRejectionReassons,
+  getFormattedLabSamplesForTracking,
+  getFormattedLabSamplesLoadedState,
+  getLabConfigurations,
+} from "src/app/store/selectors";
+import { getCurrentUserPrivileges } from "src/app/store/selectors/current-user.selectors";
+import { BarCodeModalComponent } from "../../../sample-acceptance-and-results/components/bar-code-modal/bar-code-modal.component";
 
 
 @Component({
@@ -12,68 +26,100 @@ import { getSampleTypesLoadedState, getVisitsLoadedState, getAllSampleTypes, get
   styleUrls: ["./sample-results-dashboard.component.scss"],
 })
 export class SampleResultsDashboardComponent implements OnInit {
-  visitsParameters$: Observable<any>;
-  visits$: Observable<any>;
-  sampleTypes$: Observable<any[]>;
-  labOrdersBillingInfo$: Observable<any>;
+  @Input() datesParameters: any;
+  @Input() patients: any;
+  @Input() sampleTypes: any;
+  @Input() labSamplesDepartments: any;
+  @Input() labSamplesContainers: any;
+  @Input() configs: any;
+  @Input() codedSampleRejectionReasons: any;
+  @Input() LISConfigurations: LISConfigurationsModel;
   labConfigs$: Observable<any>;
-  visitsLoadedState$: Observable<boolean>;
-  sampleTypesLoadedState$: Observable<boolean>;
-  labOrdersLoadedState$: Observable<boolean>;
-  labOrdersGroupedByPatients$: Observable<any>;
-  collectedLabOrders$: Observable<any>;
-  sampleIdentifierDetails$: Observable<any>;
-  acceptedLabOrders$: Observable<any>;
-  rejectedLabOrders$: Observable<any>;
-  labOrdersWithIntermediateResults$: Observable<any>;
-
-  labOrdersWithFirstSignOff$: Observable<any>;
-  labOrdersWithSecondSignOff$: Observable<any>;
-  visitReferences$: Observable<any>;
-
-  sampleContainers$: Observable<any>;
-  testsContainers$: Observable<any>;
   privileges$: Observable<any>;
-  codedSampleRejectionReasons$: Observable<any>;
-  labDepartments$: Observable<any>;
-  constructor(private store: Store<AppState>) {}
+  codedSampleRejectionReasons$: Observable<any[]>;
+  samplesLoadedState$: Observable<any>;
+  searchingText: string = "";
+  allSamples$: Observable<any[]>;
+  selectedDepartment: string = "";
+  status: boolean;
+  constructor(private store: Store<AppState>, private dialog: MatDialog) {}
 
   ngOnInit(): void {
-    // this.visitsParameters$ = this.store.select(getVisitsParameters);
-    this.sampleTypesLoadedState$ = this.store.select(getSampleTypesLoadedState);
-    this.visitsLoadedState$ = this.store.select(getVisitsLoadedState);
-    this.sampleTypes$ = this.store.select(getAllSampleTypes);
-    this.visits$ = this.store.select(getAllVisits);
-    this.labConfigs$ = this.store.select(getLabConfigurations);
-
-    this.labOrdersLoadedState$ = this.store.select(getLabOrdersLoadedState);
-    this.labOrdersGroupedByPatients$ = this.store.select(getAllLabOrders);
-
-    this.collectedLabOrders$ = this.store.select(getCollectedLabOrders);
-    this.sampleIdentifierDetails$ = this.store.select(
-      getSampleIdentifierDetails
+    this.store.dispatch(
+      addLabDepartments({ labDepartments: this.labSamplesDepartments })
     );
-    this.acceptedLabOrders$ = this.store.select(getAcceptedLabOrders);
-    this.rejectedLabOrders$ = this.store.select(getRejectedLabOrders);
-    this.labOrdersWithIntermediateResults$ = this.store.select(
-      getLabOrdersWithIntermediateResults
+    this.store.dispatch(
+      loadLabSamplesByCollectionDates({
+        datesParameters: this.datesParameters,
+        patients: this.patients,
+        sampleTypes: this.sampleTypes,
+        departments: this.labSamplesDepartments,
+        containers: this.labSamplesContainers,
+        configs: this.configs,
+        codedSampleRejectionReasons: this.codedSampleRejectionReasons,
+      })
     );
-    this.labOrdersWithFirstSignOff$ = this.store.select(
-      getLabOrdersWithFirstSignOff
-    );
-    this.labOrdersWithSecondSignOff$ = this.store.select(
-      getLabOrdersWithSecondSignOff
-    );
-
-    this.testsContainers$ = this.store.select(getLabTestsContainers);
-
-    this.sampleContainers$ = this.store.select(getLabSampleContainers);
-
-    this.privileges$ = this.store.select(getCurrentUserPrivileges);
 
     this.codedSampleRejectionReasons$ = this.store.select(
       getCodedSampleRejectionReassons
     );
-    this.labDepartments$ = this.store.select(getLabDepartments);
+
+    this.labConfigs$ = this.store.select(getLabConfigurations);
+
+    this.privileges$ = this.store.select(getCurrentUserPrivileges);
+
+    this.samplesLoadedState$ = this.store.select(
+      getFormattedLabSamplesLoadedState
+    );
+    this.allSamples$ = this.store
+      .select(getFormattedLabSamplesForTracking, {
+        department: this.selectedDepartment,
+        searchingText: this.searchingText,
+      })
+      .pipe(
+        map((samples) => {
+          return samples?.length && samples?.length > 0
+            ? samples.filter((sample) => sample.accepted)
+            : [];
+        })
+      );
+  }
+
+  setDepartment(department) {
+    this.selectedDepartment = department;
+    this.allSamples$ = this.store.select(getFormattedLabSamplesForTracking, {
+      department: this.selectedDepartment,
+      searchingText: this.searchingText,
+    });
+  }
+
+  onSearch(e) {
+    if (e) {
+      e.stopPropagation();
+      this.allSamples$ = this.store.select(getFormattedLabSamplesForTracking, {
+        department: this.selectedDepartment,
+        searchingText: this.searchingText,
+      });
+    }
+  }
+
+  onUpdateStatus(event: Event, sample: any): void {
+    
+    const confirmDialog = this.dialog.open(DeleteConfirmationComponent, {
+      height: "200px",
+      width: "15%",
+      data: {
+        modalTitle: "Release sample results for external use",
+        modalMessage: `You sure to release results of ${sample?.label}`,
+      },
+      disableClose: false,
+      panelClass: "custom-dialog-container",
+    });
+
+    confirmDialog.afterClosed().subscribe((res) => {
+      if (res.confirmed) {
+        this.status = !this.status;
+      }
+    });
   }
 }
