c6c8f837756bcb997a62a982082f18e8dff67f6f
josephatJ <josephatjulius24@gmail.com>
2022-09-26 09:54:07 +0300

    Add clients from other systems component

---
+++ ui/src/app/modules/laboratory/modules/sample-registration/components/clients-from-external-systems/clients-from-external-systems.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clients-from-external-systems',
+  templateUrl: './clients-from-external-systems.component.html',
+  styleUrls: ['./clients-from-external-systems.component.scss']
+})
+export class ClientsFromExternalSystemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
