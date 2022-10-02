38e98e50392968712c9c6b1e269c66a8ec567cc3
josephatJ <josephatjulius24@gmail.com>
2022-10-02 20:11:29 +0300

    Add support to manage re-order levels

--- ui/src/app/modules/store/components/index.ts
+++ ui/src/app/modules/store/components/index.ts
@@ -1,4 +1,5 @@
 import { LedgersListComponent } from "./ledgers-list/ledgers-list.component";
+import { ReOrderLevelItemsListComponent } from "./re-order-level-items-list/re-order-level-items-list.component";
 import { StockBatchListComponent } from "./stock-batch-list/stock-batch-list.component";
 import { StockInOtherUnitsComponent } from "./stock-in-other-units/stock-in-other-units.component";
 import { StockOutItemsComponent } from "./stock-out-items/stock-out-items.component";
@@ -12,4 +13,5 @@ export const stockComponents: any[] = [
   LedgersListComponent,
   StockInOtherUnitsComponent,
   TotalizeStockUnitsQuantityComponent,
+  ReOrderLevelItemsListComponent,
 ];
