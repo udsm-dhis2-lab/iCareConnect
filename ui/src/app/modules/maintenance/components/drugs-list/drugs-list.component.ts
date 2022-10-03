8b70415c4005d28be7a34a2c5456b561418a67bf
josephatJ <josephatjulius24@gmail.com>
2022-10-03 16:59:09 +0300

    Add support to list drugs

--- ui/src/app/modules/maintenance/components/drugs-list/drugs-list.component.ts
+++ ui/src/app/modules/maintenance/components/drugs-list/drugs-list.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { DrugsService } from "src/app/shared/resources/drugs/services/drugs.service";
 
 @Component({
-  selector: 'app-drugs-list',
-  templateUrl: './drugs-list.component.html',
-  styleUrls: ['./drugs-list.component.scss']
+  selector: "app-drugs-list",
+  templateUrl: "./drugs-list.component.html",
+  styleUrls: ["./drugs-list.component.scss"],
 })
 export class DrugsListComponent implements OnInit {
+  @Input() conceptUuid: string;
+  drugs$: Observable<any>;
+  startIndex: number = 0;
+  limit: number = 10;
 
-  constructor() { }
+  page: number = 1;
+  constructor(private drugService: DrugsService) {}
 
   ngOnInit(): void {
+    this.getDrugs();
   }
 
+  getDrugs(): void {
+    if (this.conceptUuid) {
+      this.drugs$ = this.drugService.getDrugsUsingConceptUuid(this.conceptUuid);
+    } else {
+      this.drugs$ = this.drugService.getAllDrugs({
+        startIndex: this.startIndex,
+        limit: this.limit,
+      });
+    }
+  }
+
+  onGetList(event: Event, actionType: string): void {
+    event.stopPropagation();
+    this.page = actionType === "next" ? this.page + 1 : this.page - 1;
+    this.startIndex = this.limit * this.page - 1 + 1;
+    this.getDrugs();
+  }
 }
--- ui/src/app/modules/maintenance/components/generic-drugs-list/generic-drugs-list.component.ts
+++ ui/src/app/modules/maintenance/components/generic-drugs-list/generic-drugs-list.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Observable } from "rxjs";
 import { ConceptsService } from "src/app/shared/resources/concepts/services/concepts.service";
+import { ConceptGet } from "src/app/shared/resources/openmrs";
 import { AddNewGenericDrugModalComponent } from "../../modals/add-new-generic-drug-modal/add-new-generic-drug-modal.component";
+import { DrugListModalComponent } from "../../modals/drug-list-modal/drug-list-modal.component";
 
 @Component({
   selector: "app-generic-drugs-list",
@@ -30,8 +32,14 @@ export class GenericDrugsListComponent implements OnInit {
     });
   }
 
-  onViewDrugs(event: Event): void {
+  onViewDrugs(event: Event, concept: ConceptGet): void {
     // event.stopPropagation();
+    this.dialog.open(DrugListModalComponent, {
+      width: "50%",
+      data: {
+        concept,
+      },
+    });
   }
 
   onEdit(event: Event): void {
