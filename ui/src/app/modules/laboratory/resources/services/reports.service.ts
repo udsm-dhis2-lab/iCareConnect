cf4c48ae478ebad0bafacc520c7f1abca54f1cb6
jonas1015 <jonasgsmart1015@gmail.com>
2022-10-07 16:48:20 +0300

    Initial fixes in lab reports

--- ui/src/app/modules/laboratory/resources/services/reports.service.ts
+++ ui/src/app/modules/laboratory/resources/services/reports.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { from, Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { OpenmrsHttpClientService } from 'src/app/shared/modules/openmrs-http-client/services/openmrs-http-client.service';
 import { Api, UserGet, UserGetFull } from 'src/app/shared/resources/openmrs';
 import * as _ from 'lodash';
@@ -12,11 +12,9 @@ export class LabReportsService {
   constructor(private api: Api, private httpClient: OpenmrsHttpClientService) {}
 
   runDataSet(dataSetId, selectionDates?: any): Observable<any> {
-    return this.httpClient
-      .get(
-        `reportingrest/dataSet/${dataSetId}?startDate=${selectionDates?.startDate}&endDate=${selectionDates?.endDate}`
-      )
-      .pipe(
+    return this.httpClient.get(
+      `reportingrest/dataSet/${dataSetId}?startDate=${selectionDates?.startDate}&endDate=${selectionDates?.endDate}`
+      ).pipe(
         map((response) => {
           let keys = _.map(response?.metadata?.columns, (col) => col?.name);
 
@@ -28,7 +26,8 @@ export class LabReportsService {
 
             return newRow;
           });
-        })
-      );
+        }),
+        catchError((error) => of(error))
+      )
   }
 }
