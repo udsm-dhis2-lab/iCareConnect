6ddf011f208169949774220237bbb461fd359008
josephatJ <josephatjulius24@gmail.com>
2022-10-08 17:04:04 +0300

    Add support to capture preferred modules

--- ui/src/app/modules/maintenance/containers/manage-users/manage-users.component.ts
+++ ui/src/app/modules/maintenance/containers/manage-users/manage-users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
+import { SystemSettingsService } from "src/app/core/services/system-settings.service";
 import { SystemUsersService } from "src/app/core/services/system-users.service";
 
 @Component({
@@ -11,15 +12,20 @@ import { SystemUsersService } from "src/app/core/services/system-users.service";
 export class ManageUsersComponent implements OnInit {
   userId: string;
   user$: Observable<any>;
+  systemModules$: Observable<any[]>;
   constructor(
     private route: ActivatedRoute,
-    private usersService: SystemUsersService
+    private usersService: SystemUsersService,
+    private systemSettingsService: SystemSettingsService
   ) {}
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.queryParams["id"];
-    console.log(this.userId);
-
-    this.user$ = this.usersService.getUserById(this.userId);
+    this.systemModules$ = this.systemSettingsService.getSystemSettingsByKey(
+      `iCare.general.systemSettings.UIModules`
+    );
+    if (this.userId) {
+      this.user$ = this.usersService.getUserById(this.userId);
+    }
   }
 }
