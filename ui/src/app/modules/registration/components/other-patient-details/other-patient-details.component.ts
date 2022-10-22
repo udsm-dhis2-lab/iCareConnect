c52df41931b5e2e0b7e7d070bd5a201554d75c9c
josephatJ <josephatjulius24@gmail.com>
2022-10-22 23:42:40 +0300

    Add support to show client ID from registration details

--- ui/src/app/modules/registration/components/other-patient-details/other-patient-details.component.ts
+++ ui/src/app/modules/registration/components/other-patient-details/other-patient-details.component.ts
@@ -1,18 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { VisitsService } from 'src/app/shared/resources/visits/services';
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { VisitsService } from "src/app/shared/resources/visits/services";
 
 @Component({
-  selector: 'app-other-patient-details',
-  templateUrl: './other-patient-details.component.html',
-  styleUrls: ['./other-patient-details.component.scss'],
+  selector: "app-other-patient-details",
+  templateUrl: "./other-patient-details.component.html",
+  styleUrls: ["./other-patient-details.component.scss"],
 })
 export class OtherPatientDetailsComponent implements OnInit {
   @Input() patient: any;
   patientVisit$: Observable<any>;
+  ID: string;
   constructor(private visitService: VisitsService) {}
 
   ngOnInit(): void {
+    // TODO: Softcode ID reference
+    this.ID = (this.patient?.person?.attributes?.filter(
+      (attribute) => attribute?.attributeType?.display === "ID"
+    ) || [])[0]?.value;
     this.patientVisit$ = this.visitService.getLastPatientVisit(
       this.patient?.uuid
     );
