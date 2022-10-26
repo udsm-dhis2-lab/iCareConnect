7a4b6b2a8f06db9d24df9720cf3032bfdbad3dbe
josephatJ <josephatjulius24@gmail.com>
2022-10-26 14:04:39 +0300

    Fix results printing issue

--- ui/src/app/store/selectors/new-lab-samples.selectors.ts
+++ ui/src/app/store/selectors/new-lab-samples.selectors.ts
@@ -571,12 +571,80 @@ export const getCompletedLabSamples = createSelector(
   }
 );
 
+export const getPatientWithResults = (
+  department: string,
+  searchingText: string
+) =>
+  createSelector(getAllFormattedLabSamples, (samples: any[]) => {
+    const filteredSamplesWithResults =
+      _.filter(
+        _.orderBy(
+          _.filter(samples, (sample) => {
+            const ordersWithResults = getOrdersWithResults(sample?.orders);
+            if (sample?.accepted && ordersWithResults.length) {
+              return sample;
+            }
+          }),
+          ["dateCreated", "priorityOrderNumber"],
+          ["asc", "asc"]
+        ),
+        (sample) => {
+          if (!searchingText && !department) {
+            return sample;
+          } else if (searchingText && department) {
+            if (
+              sample?.searchingText
+                ?.toLowerCase()
+                .indexOf(searchingText.toLowerCase()) > -1 &&
+              sample?.department?.departmentName
+                .toLowerCase()
+                .indexOf(department?.toLowerCase()) > -1
+            ) {
+              return sample;
+            }
+          } else if (searchingText && !department) {
+            if (
+              sample?.searchingText
+                ?.toLowerCase()
+                .indexOf(searchingText.toLowerCase()) > -1
+            ) {
+              return sample;
+            }
+          } else if (!searchingText && department) {
+            if (
+              sample?.department?.departmentName
+                .toLowerCase()
+                .indexOf(department?.toLowerCase()) > -1
+            ) {
+              return sample;
+            }
+          }
+        }
+      ) || [];
+    const groupedByMRN = _.groupBy(filteredSamplesWithResults, "mrn");
+
+    return _.map(Object.keys(groupedByMRN), (key) => {
+      const samplesKeyedByDepartments = _.groupBy(
+        groupedByMRN[key],
+        "departmentName"
+      );
+      return {
+        mrn: key,
+        patient: groupedByMRN[key][0]?.patient,
+        departments: _.map(Object.keys(samplesKeyedByDepartments), (dep) => {
+          return {
+            departmentName: dep,
+            samples: samplesKeyedByDepartments[dep],
+          };
+        }),
+      };
+    });
+  });
+
 export const getPatientsWithCompletedLabSamples = createSelector(
   getAllFormattedLabSamples,
   getLISConfigurations,
   (samples, lisConfigs, props) => {
-    // console.log('SAMP ', samples);
-
     const filteredCompletedSamples =
       _.filter(
         _.orderBy(
@@ -628,12 +696,7 @@ export const getPatientsWithCompletedLabSamples = createSelector(
           }
         }
       ) || [];
-
-    // console.log('filtered completed samples :: ', filteredCompletedSamples);
-
     const groupedByMRN = _.groupBy(filteredCompletedSamples, "mrn");
-
-    // console.log('grouped by mrn samples :: ', groupedByMRN);
 
     return _.map(Object.keys(groupedByMRN), (key) => {
       const samplesKeyedByDepartments = _.groupBy(
