43ac6a356040b83efc86e319985f78a34a90a964
josephatJ <josephatjulius24@gmail.com>
2022-10-26 16:18:09 +0300

    Fix missing report issue

--- ui/src/app/modules/laboratory/modules/lab-reports/reports.module.ts
+++ ui/src/app/modules/laboratory/modules/lab-reports/reports.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { ReportsRoutingModule } from './reports-routing.module';
-import { components } from './components';
-import { ReportsHomeComponent } from './reports-home/reports-home.component';
-import { LabReportsComponent } from './components/lab-reports/lab-reports.component';
-import { ReportsDashboardComponent } from './pages/reports-dashboard/reports-dashboard.component';
-import { CustomReportsComponent } from './components/custom-reports/custom-reports.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SharedModule } from "src/app/shared/shared.module";
+import { ReportsRoutingModule } from "./reports-routing.module";
+import { components } from "./components";
+import { ReportsHomeComponent } from "./reports-home/reports-home.component";
+import { LabReportsComponent } from "./components/lab-reports/lab-reports.component";
+import { ReportsDashboardComponent } from "./pages/reports-dashboard/reports-dashboard.component";
+import { CustomReportsComponent } from "./components/custom-reports/custom-reports.component";
+import { SearchingItemPipe } from "./pipes/searching-item.pipe";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { CustomReportsComponent } from './components/custom-reports/custom-repor
     LabReportsComponent,
     ReportsDashboardComponent,
     CustomReportsComponent,
-    
+    SearchingItemPipe,
   ],
   imports: [CommonModule, ReportsRoutingModule, SharedModule],
 })
