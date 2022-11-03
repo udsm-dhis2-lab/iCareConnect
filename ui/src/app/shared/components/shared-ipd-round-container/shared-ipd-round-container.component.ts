1780f8e1592caab206c77d0ae1b08dafc38b40c2
jonas1015 <jonasgsmart1015@gmail.com>
2022-11-03 20:59:28 +0300

    More IPD Round Implementation

--- ui/src/app/shared/components/shared-ipd-round-container/shared-ipd-round-container.component.ts
+++ ui/src/app/shared/components/shared-ipd-round-container/shared-ipd-round-container.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
+import { sortBy } from "lodash";
 import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
 import { saveObservations } from "src/app/store/actions/observation.actions";
 import { AppState } from "src/app/store/reducers";
 import { getGroupedObservationByDateAndTimeOfIPDRounds } from "src/app/store/selectors/observation.selectors";
+import { FormService } from "../../modules/form/services/form.service";
 import { Visit } from "../../resources/visits/models/visit.model";
 
 @Component({
@@ -17,12 +20,34 @@ export class SharedIPDRoundContainerComponent implements OnInit {
   @Input() location: any;
   @Input() observations: any[];
   observationsGroupedByIPDRounds$: Observable<any>;
-  constructor(private store: Store<AppState>) {}
+  form$: Observable<any>;
+  errors: any[] = [];
+  constructor(
+    private store: Store<AppState>,
+    private formService: FormService
+  ) {}
 
   ngOnInit(): void {
     // console.log(this.activeVisit);
     // console.log(this.conceptUuid);
     // console.log(this.observations);
+
+    // TODO: Visit Notes form to be softcoded
+    this.form$ = this.formService
+      .getCustomeOpenMRSForm("c75f120a-04ec-11e3-8780-2b40bef9a44b")
+      .pipe(
+        map((formObject) => {
+          if(!formObject?.error){
+            return formObject;
+          }
+          if(formObject?.error){
+            this.errors = [
+              ...this.errors,
+              formObject?.errors
+            ]
+          }
+        })
+      );
     console.log(this.observations[this.conceptUuid]);
     // First create round zero provided when no any round has been made
     if (!this.observations[this.conceptUuid]) {
