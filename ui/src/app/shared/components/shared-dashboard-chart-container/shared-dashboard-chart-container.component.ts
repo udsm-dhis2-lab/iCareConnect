54e6a802bb67a615294d11b4abce0144081bcf33
josephatJ <josephatjulius24@gmail.com>
2022-11-10 18:15:28 +0300

    Add chart item - coded (Consider softcoding)

--- ui/src/app/shared/components/shared-dashboard-chart-container/shared-dashboard-chart-container.component.ts
+++ ui/src/app/shared/components/shared-dashboard-chart-container/shared-dashboard-chart-container.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { SamplesService } from "src/app/modules/laboratory/resources/services/samples.service";
 
 @Component({
-  selector: 'app-shared-dashboard-chart-container',
-  templateUrl: './shared-dashboard-chart-container.component.html',
-  styleUrls: ['./shared-dashboard-chart-container.component.scss']
+  selector: "app-shared-dashboard-chart-container",
+  templateUrl: "./shared-dashboard-chart-container.component.html",
+  styleUrls: ["./shared-dashboard-chart-container.component.scss"],
 })
 export class SharedDashboardChartContainerComponent implements OnInit {
-
-  constructor() { }
+  @Input() datesParams: any;
+  samples$: Observable<any>;
+  constructor(private sampleService: SamplesService) {}
 
   ngOnInit(): void {
+    this.samples$ = this.sampleService.getAggregatedSamplesByDifferentStatuses(
+      ["ACCEPTED", "REJECTED", "COMPLETED"],
+      this.datesParams?.startDate,
+      this.datesParams?.endDate
+    );
   }
-
 }
