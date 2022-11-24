c6f13fb9c062585877a87e3ec32c32efe970795d
Dkiba <kibahiladennis@gmail.com>
2022-11-24 11:04:35 +0300

    modified an api to allow voiding orders

--- omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/ICareController.java
+++ omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/ICareController.java
@@ -58,6 +58,9 @@ public class ICareController {
 	@Autowired
 	BillingService billingService;
 	
+	@Autowired
+	OrderService orderService;
+	
 	/** Logger for this class and subclasses */
 	protected final Log log = LogFactory.getLog(getClass());
 	
@@ -650,16 +653,23 @@ public class ICareController {
 	
 	@RequestMapping(value = "voidorder", method = RequestMethod.POST)
 	@ResponseBody
-	public Map<String, Object> voidOrder(@RequestBody Map<String, Object> voidObj) throws IOException {
+	public Map<String, Object> voidOrder(@RequestBody Map<String, Object> voidObj) {
 		Map<String, Object> returnResponse = new HashMap<>();
-		String response;
-		try {
-			response = iCareService.voidOrder((String) voidObj.get("uuid"), (String) voidObj.get("voidReason"));
+//		String response;
+//		try {
+//			response = iCareService.voidOrder((String) voidObj.get("uuid"), (String) voidObj.get("voidReason"));
+//		}
+//		catch (IOException e) {
+//			throw new RuntimeException(e);
+//		}
+
+		Order order = orderService.getOrderByUuid((String) voidObj.get("uuid"));
+		if (order == null){
+			throw new APIException("The order uuid does not exist");
 		}
-		catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		returnResponse.put("uuid", response);
+		Order voidedorder =orderService.voidOrder(order,(String) voidObj.get("voidReason"));
+
+		returnResponse.put("uuid", voidedorder.getUuid());
 		return returnResponse;
 	}
 }
