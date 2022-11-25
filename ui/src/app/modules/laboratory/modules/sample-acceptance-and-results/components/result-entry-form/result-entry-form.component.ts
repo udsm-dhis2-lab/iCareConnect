8aee08d8f46f05cb57742e398099dde4db6c9178
josephatJ <josephatjulius24@gmail.com>
2022-11-25 09:43:39 +0300

    Change parameter entry form and add support to authorize multiple results

---
+++ ui/src/app/modules/laboratory/modules/sample-acceptance-and-results/components/result-entry-form/result-entry-form.component.ts
@@ -0,0 +1,70 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Dropdown } from "src/app/shared/modules/form/models/dropdown.model";
+import { Field } from "src/app/shared/modules/form/models/field.model";
+import { FormValue } from "src/app/shared/modules/form/models/form-value.model";
+import { Textbox } from "src/app/shared/modules/form/models/text-box.model";
+
+@Component({
+  selector: "app-result-entry-form",
+  templateUrl: "./result-entry-form.component.html",
+  styleUrls: ["./result-entry-form.component.scss"],
+})
+export class ResultEntryFormComponent implements OnInit {
+  @Input() parameter: any;
+  @Input() hasMultipleAnswers: boolean;
+  @Input() value: any;
+  @Input() disabled: boolean;
+  formField: Field<string>;
+  @Output() formData: EventEmitter<any> = new EventEmitter<any>();
+  constructor() {}
+
+  ngOnInit(): void {
+    this.formField =
+      this.parameter?.datatype?.display === "Numeric"
+        ? new Textbox({
+            id: this.parameter?.uuid,
+            key: this.parameter?.uuid,
+            label: this.parameter?.display,
+            type: "number",
+            value: this.value,
+            disabled: this.disabled,
+            min: this.parameter?.min,
+            max: this.parameter?.max,
+            required: true,
+          })
+        : this.parameter?.datatype?.display === "Coded"
+        ? new Dropdown({
+            id: this.parameter?.uuid,
+            key: this.parameter?.uuid,
+            label: this.parameter?.display,
+            type: "number",
+            value: this.value,
+            disabled: this.disabled,
+            multiple: this.hasMultipleAnswers,
+            options: this.parameter?.answers?.map((answer) => {
+              return {
+                value: answer?.uuid,
+                key: answer?.uuid,
+                name: answer?.display,
+                label: answer?.display,
+              };
+            }),
+            min: this.parameter?.min,
+            max: this.parameter?.max,
+            required: true,
+          })
+        : new Textbox({
+            id: this.parameter?.uuid,
+            key: this.parameter?.uuid,
+            label: this.parameter?.display,
+            type: "text",
+            value: this.value,
+            disabled: this.disabled,
+            required: true,
+          });
+  }
+
+  onFormUpdate(formValue: FormValue): void {
+    this.formData.emit(formValue?.getValues()[this.parameter?.uuid]?.value);
+  }
+}
