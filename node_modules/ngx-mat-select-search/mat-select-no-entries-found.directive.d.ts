270e97790d0bfc63ae93c6a9a146f2065969e68f
josephatJ <josephatjulius24@gmail.com>
2022-11-28 13:22:05 +0300

    Add multiple results capture select

---
+++ node_modules/ngx-mat-select-search/mat-select-no-entries-found.directive.d.ts
@@ -0,0 +1,14 @@
+import * as i0 from "@angular/core";
+/**
+ * Directive for providing a custom no entries found element.
+ * e.g.
+ * <ngx-mat-select-search [formControl]="bankFilterCtrl">
+ *   <span ngxMatSelectNoEntriesFound>
+ *     No entries found <button>Add</button>
+ *   </span>
+ * </ngx-mat-select-search>
+ */
+export declare class MatSelectNoEntriesFoundDirective {
+    static ɵfac: i0.ɵɵFactoryDeclaration<MatSelectNoEntriesFoundDirective, never>;
+    static ɵdir: i0.ɵɵDirectiveDeclaration<MatSelectNoEntriesFoundDirective, "[ngxMatSelectNoEntriesFound]", never, {}, {}, never, never, false, never>;
+}
