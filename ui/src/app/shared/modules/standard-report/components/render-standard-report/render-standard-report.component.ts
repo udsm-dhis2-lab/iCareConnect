df9d2c3a288e3caf65e6f15e65e34aef5abf4450
josephatJ <josephatjulius24@gmail.com>
2022-12-01 11:45:33 +0300

    Add support to load datasets data ready for rendering

--- ui/src/app/shared/modules/standard-report/components/render-standard-report/render-standard-report.component.ts
+++ ui/src/app/shared/modules/standard-report/components/render-standard-report/render-standard-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { DatasetReportsService } from "../../services/dataset-reports.service";
 
 @Component({
   selector: "lib-render-standard-report",
@@ -7,7 +9,15 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class RenderStandardReportComponent implements OnInit {
   @Input() report: any;
-  constructor() {}
+  @Input() parameters: any;
+  evaluatedDataSetsReport$: Observable<any>;
+  constructor(private datasetReportService: DatasetReportsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.evaluatedDataSetsReport$ =
+      this.datasetReportService.getEvaluatedDatasetReport(
+        this.report?.queries?.map((query) => query?.uuid),
+        this.parameters
+      );
+  }
 }
