41d1caf861beb92886b6e04e97f6d43ec61e80be
SalumWaKwanza <salumpatrick1404@gmail.com>
2022-12-13 19:42:35 +0300

    fixing issue 53

--- ui/src/app/modules/reports/components/report-period-filter/report-period-filter.component.spec.ts
+++ ui/src/app/modules/reports/components/report-period-filter/report-period-filter.component.spec.ts
@@ -5,7 +5,7 @@ import { ReportPeriodFilterComponent } from './report-period-filter.component';
 describe('ReportPeriodFilterComponent', () => {
   let component: ReportPeriodFilterComponent;
   let fixture: ComponentFixture<ReportPeriodFilterComponent>;
-
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ReportPeriodFilterComponent ]
