94ef99a52af67bdf8a8cbce4dda09a3231d74967
ressyjohn <ressyjohn19@gmail.com>
2022-12-13 10:23:54 +0300

    fixed bugs

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { MatLegacyDialog as MatDialog } from "@angular/material/legacy-dialog";
+import { Component, OnDestroy, OnInit, VERSION } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { ActivatedRoute } from "@angular/router";
 import { select, Store } from "@ngrx/store";
-import { Observable, timer } from "rxjs";
+import { Observable, fromEvent, merge, of, Subscription, timer } from "rxjs";
+import { map } from 'rxjs/operators';
 import { LoginDialogComponent } from "./core/dialogs/login-dialog/login-dialog.component";
 import { AuthService } from "./core/services/auth.service";
 import { loadDHIS2ReportsConfigs } from "./store/actions";
@@ -14,11 +15,14 @@ import { getIfNonLoginRoute } from "./store/selectors";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
   title = "iCare";
 
   isNonLoginRoute$: Observable<boolean>;
   isDialogOpen: boolean;
+  networkStatus: boolean = false;
+  networkStatus$: Subscription = Subscription.EMPTY;
   constructor(
     private store: Store<AppState>,
     private dialog: MatDialog,
@@ -27,10 +31,31 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.checkNetworkStatus();
     this.monitorSession();
     this.isNonLoginRoute$ = this.store.pipe(select(getIfNonLoginRoute));
   }
 
+  ngOnDestroy(): void {
+    this.networkStatus$.unsubscribe();
+}
+
+// To check for internet connection stability
+checkNetworkStatus() {
+  this.networkStatus = navigator.onLine;
+  this.networkStatus$ = merge(
+    of(null),
+    fromEvent(window, 'online'),
+fromEvent(window, 'offline')
+  )
+  .pipe(map(() => navigator.onLine))
+      .subscribe(status => {
+            console.log('status', status);
+        this.networkStatus = status;
+      });
+      window.alert(this.networkStatus
+        ? 'IS CONNECTED': 'NOT CONNECTED');
+    }
   monitorSession() {
     timer(2000, 10000).subscribe(() => {
       this.auth.session().subscribe((session) => {
