ff1893fde68ff743315a5cd204e7ae0b5914aa1a
josephatJ <josephatjulius24@gmail.com>
2022-12-14 11:07:26 +0300

    Add support for updating results by setting instrument used

--- omods/core/api/src/main/java/org/openmrs/module/icare/laboratory/dao/ResultDAO.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/laboratory/dao/ResultDAO.java
@@ -1,10 +1,12 @@
 package org.openmrs.module.icare.laboratory.dao;
 
 import org.hibernate.Query;
+import org.openmrs.Concept;
 import org.openmrs.api.db.hibernate.DbSession;
 import org.openmrs.module.icare.core.dao.BaseDAO;
 import org.openmrs.module.icare.laboratory.models.Result;
 import org.openmrs.module.icare.laboratory.models.Sample;
+import org.openmrs.module.icare.laboratory.models.TestRangeConfig;
 import org.springframework.stereotype.Repository;
 
 import java.util.Calendar;
@@ -18,5 +20,25 @@ public class ResultDAO extends BaseDAO<Result> {
         Query query = session.createQuery(queryStr);
         query.setParameter("id", id);
         return  (Result)query.list().get(0);
+    }
+
+    public Result updateResultsBySettingInstrument(Result result, Concept instrument) {
+        DbSession session = this.getSession();
+
+        String queryStr = "UPDATE Result set " + "instrument = :instrument"
+//                + "changedBy = :changedBy, " + "dateChanged = :dateChanged"
+                + " WHERE uuid =:uuid";
+
+        Query query = session.createQuery(queryStr);
+        query.setParameter("instrument", instrument);
+        query.setParameter("uuid", result.getUuid());
+        Integer success = query.executeUpdate();
+        if (success == 1) {
+            Result newResult = result;
+            newResult.setInstrument(instrument);
+            return newResult;
+        } else {
+            return null;
+        }
     }
 }
