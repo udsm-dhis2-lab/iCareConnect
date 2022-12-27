a726c4011053b86abc964b303a67a027190db5b5
josephatJ <josephatjulius24@gmail.com>
2022-12-27 09:58:57 +0300

    Add support to save worksheet config details

--- ui/src/app/modules/laboratory/modules/settings/components/create-worksheet/create-worksheet.component.ts
+++ ui/src/app/modules/laboratory/modules/settings/components/create-worksheet/create-worksheet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { Dropdown } from "src/app/shared/modules/form/models/dropdown.model";
 import { FormValue } from "src/app/shared/modules/form/models/form-value.model";
 import { TextArea } from "src/app/shared/modules/form/models/text-area.model";
 import { Textbox } from "src/app/shared/modules/form/models/text-box.model";
@@ -10,6 +11,7 @@ import { Textbox } from "src/app/shared/modules/form/models/text-box.model";
 })
 export class CreateWorksheetComponent implements OnInit {
   worksheetFields: any[];
+  @Output() formValues: EventEmitter<FormValue> = new EventEmitter<FormValue>();
   constructor() {}
 
   ngOnInit(): void {
@@ -52,10 +54,32 @@ export class CreateWorksheetComponent implements OnInit {
         min: 1,
         required: true,
       }),
+      new Dropdown({
+        id: "testorder",
+        key: "testorder",
+        label: "Test Order",
+        required: false,
+        options: [],
+        conceptClass: "Test",
+        searchControlType: "concept",
+        searchTerm: "TEST_ORDERS",
+        shouldHaveLiveSearchForDropDownFields: true,
+      }),
+      new Dropdown({
+        id: "instrument",
+        key: "instrument",
+        label: "Instrument",
+        required: false,
+        options: [],
+        conceptClass: "LIS instrument",
+        searchControlType: "concept",
+        searchTerm: "LIS_INSTRUMENT",
+        shouldHaveLiveSearchForDropDownFields: true,
+      }),
     ];
   }
 
   onFOrmUpdate(formValue: FormValue): void {
-    console.log(formValue.getValues());
+    this.formValues.emit(formValue);
   }
 }
