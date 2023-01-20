53ecb222c256e5d66a49ba4828618bbb8769b509
josephatJ <josephatjulius24@gmail.com>
2023-01-20 16:45:11 +0300

    Improve period selection and fix multiple period selection issue

--- ui/src/app/shared/modules/period-filter/containers/period-filter/period-filter.component.ts
+++ ui/src/app/shared/modules/period-filter/containers/period-filter/period-filter.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { getFormattedPeriodsByPeriodType } from "src/app/shared/helpers/format-dates-types.helper";
 import { orderBy } from "lodash";
+import { Dropdown } from "../../../form/models/dropdown.model";
+import { FormValue } from "../../../form/models/form-value.model";
 
 @Component({
   selector: "ngx-period-filter",
@@ -18,9 +20,24 @@ export class PeriodFilterComponent implements OnInit {
   currentYear: number;
   selectedPeriods: any[] = [];
   @Output() selectedItems: EventEmitter<any[]> = new EventEmitter<any[]>();
+  periodTypesFormField: any;
+  topHeaderHeight: number = 52;
   constructor() {}
 
   ngOnInit(): void {
+    this.periodTypesFormField = new Dropdown({
+      id: "periodType",
+      key: "periodType",
+      label: "Period type",
+      options: this.periodType?.categories?.map((type) => {
+        return {
+          id: type?.id,
+          key: type?.id,
+          label: type?.name,
+          value: type?.id,
+        };
+      }),
+    });
     this.currentYear = new Date().getFullYear();
     this.currentYearlyPe = this.currentYear;
     this.periodTypeId = this.periodType?.default?.id;
@@ -28,6 +45,10 @@ export class PeriodFilterComponent implements OnInit {
       ? []
       : this.selectedPeriodItems;
     this.generatePeriodList();
+  }
+
+  onFormUpdate(formValue: FormValue): void {
+    console.log(formValue.getValues());
   }
 
   generatePeriodList(): void {
