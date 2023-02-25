93a504ce46e01bb6cf778ac5d1b6b80644407a7e
josephatJ <josephatjulius24@gmail.com>
2023-02-25 14:08:10 +0300

    Fix no results api issue: Returning samples not accepted

--- omods/core/api/src/main/java/org/openmrs/module/icare/laboratory/dao/SampleDAO.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/laboratory/dao/SampleDAO.java
@@ -148,7 +148,7 @@ public class SampleDAO extends BaseDAO<Sample> {
 				} else {
 					queryStr += " AND ";
 				}
-				queryStr += " sp NOT IN (SELECT DISTINCT sst.sample FROM SampleStatus sst WHERE (sst.category='HAS_RESULTS'  OR  lower(sst.category) LIKE 'reject%'))  ";
+				queryStr += " sp NOT IN (SELECT DISTINCT sst.sample FROM SampleStatus sst WHERE (sst.category='HAS_RESULTS'  OR  lower(sst.category) LIKE 'reject%'))   AND sp IN (SELECT DISTINCT sst.sample FROM SampleStatus sst WHERE (sst.category='ACCEPTED'))";
 			} else {
 				
 				if (!queryStr.contains("WHERE")) {
@@ -463,7 +463,7 @@ public class SampleDAO extends BaseDAO<Sample> {
 				} else {
 					queryStr += " AND ";
 				}
-				queryStr += " sp NOT IN (SELECT DISTINCT sst.sample FROM SampleStatus sst WHERE (sst.category='HAS_RESULTS'  OR  lower(sst.category) LIKE 'reject%')) ";
+				queryStr += " sp NOT IN (SELECT DISTINCT sst.sample FROM SampleStatus sst WHERE (sst.category='HAS_RESULTS'  OR  lower(sst.category) LIKE 'reject%')) AND sp IN (SELECT DISTINCT sst.sample FROM SampleStatus sst WHERE (sst.category='ACCEPTED'))";
 			} else {
 				
 				if (!queryStr.contains("WHERE")) {
