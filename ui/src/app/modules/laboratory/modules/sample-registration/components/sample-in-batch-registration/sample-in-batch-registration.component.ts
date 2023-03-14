b23e38827c9bbe5ecc6ef14a99572057eb054020
jonas1015 <jonasgsmart1015@gmail.com>
2023-03-14 16:10:26 +0300

    Added tests when printing barcodes

--- ui/src/app/modules/laboratory/modules/sample-registration/components/sample-in-batch-registration/sample-in-batch-registration.component.ts
+++ ui/src/app/modules/laboratory/modules/sample-registration/components/sample-in-batch-registration/sample-in-batch-registration.component.ts
@@ -3018,26 +3018,25 @@ export class SampleInBatchRegistrationComponent
       })
       .afterClosed()
       .subscribe((results) => {
-        if (results) {
-          let message = this.barcodeSettings?.barcode
-            ?.split("{{SampleID}}")
-            .join(results?.sampleData?.label);
-          message = message
-            .split("{{PatientNames}}")
-            .join(
-              `${results?.sampleData?.patient?.givenName} ${results?.sampleData?.patient?.familyName}`
-            );
-          message = message
-            ?.split("{{Date}}")
-            .join(
-              formatDateToYYMMDD(new Date(results?.sampleData?.created), true)
-            );
+        if(results){
+           let tests = []
+          results?.sampleData?.orders?.forEach((order) => {
+            tests = [
+              ...tests,
+              order?.order?.shortName?.split("TEST_ORDERS:")?.join("")
+            ]
+          })
+          let message = this.barcodeSettings?.barcode?.split("{{SampleID}}").join(results?.sampleData?.label);
+          message = message.split("{{PatientNames}}").join(`${results?.sampleData?.patient?.givenName} ${results?.sampleData?.patient?.familyName}`);
+          message = message?.split("{{Date}}").join(formatDateToYYMMDD(new Date(results?.sampleData?.created), true));
           message = message?.split("{{Storage}}").join("");
-          message = message?.split("{{Tests}}").join("");
-          this.connection.next({
-            Message: message,
-            Type: "print",
-          });
+          message = message?.split("{{Tests}}").join(tests?.join(","));
+          this.connection.next(
+            {
+              Message: message, 
+              Type: "print"
+            }
+          )
         }
       });
   }
