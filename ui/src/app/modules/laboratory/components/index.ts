9635fac989d2e1ebcd89b7bee584a6e92d828aad
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-03-21 10:44:25 +0300

    Change fields selection form field

--- ui/src/app/modules/laboratory/components/index.ts
+++ ui/src/app/modules/laboratory/components/index.ts
@@ -1,5 +1,6 @@
 import { DefineAdditionalDataFieldsComponent } from "./define-additional-data-fields/define-additional-data-fields.component";
 import { ManageAdditionalDataFieldsComponent } from "./manage-additional-data-fields/manage-additional-data-fields.component";
+import { MultipleResultsEntryComponent } from "./multiple-results-entry/multiple-results-entry.component";
 import { SamplesToCollectComponent } from "./samples-to-collect/samples-to-collect.component";
 import { SharedAssociatedFieldResultsEntryComponent } from "./shared-associated-field-results-entry/shared-associated-field-results-entry.component";
 import { SharedSamplesSummaryDashboardComponent } from "./shared-samples-summary-dashboard/shared-samples-summary-dashboard.component";
@@ -10,4 +11,5 @@ export const labSharedComponents: any[] = [
   DefineAdditionalDataFieldsComponent,
   SharedAssociatedFieldResultsEntryComponent,
   SamplesToCollectComponent,
+  MultipleResultsEntryComponent,
 ];
