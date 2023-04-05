6d9cd02c6908079813f11ef526b299ac1eaccdc8
Dkiba <kibahiladennis@gmail.com>
2023-04-05 10:39:36 +0300

    added visit when returning payment

--- omods/core/api/src/main/java/org/openmrs/module/icare/core/Item.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/core/Item.java
@@ -112,15 +112,15 @@ public class Item extends BaseOpenmrsData {
 			itemMap.put("concept", concept);
 		}
 		if (this.getDrug() != null) {
+			
 			HashMap<String, Object> drug = new HashMap<String, Object>();
 			drug.put("uuid", this.getDrug().getUuid());
 			drug.put("display", this.getDrug().getDisplayName());
 			
 			Map<String, Object> conceptObject = new HashMap<String, Object>();
-			if (this.getDrug() != null) {
-				conceptObject.put("uuid", this.getDrug().getConcept().getUuid());
-				conceptObject.put("display", this.getDrug().getConcept().getDisplayString());
-			}
+			conceptObject.put("uuid", this.getDrug().getConcept().getUuid());
+			conceptObject.put("display", this.getDrug().getConcept().getDisplayString());
+			
 			drug.put("concept", conceptObject);
 			itemMap.put("drug", drug);
 		}
