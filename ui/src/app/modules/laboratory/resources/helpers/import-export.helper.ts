83eb96484c6617fd2bde2c2b8268eccb28d42653
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-04-28 14:36:14 +0300

    Add ui contents for import export functionalities

---
+++ ui/src/app/modules/laboratory/resources/helpers/import-export.helper.ts
@@ -0,0 +1,12 @@
+export function generateEmptyExcelTemplate(): any {
+  return null;
+}
+
+export function generateExcelTemplateWithSamples(samples: any[]): any {
+  return null;
+}
+
+export function processImportedExcelFile(file: any): any {
+  console.log(file);
+  return "TESTING";
+}
