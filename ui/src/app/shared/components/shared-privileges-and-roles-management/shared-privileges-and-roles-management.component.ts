0e594a952c29ba810e6c0e6484e016cb55f00508
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-05-12 14:13:35 +0300

    Add access control to other maintenance modules

--- ui/src/app/shared/components/shared-privileges-and-roles-management/shared-privileges-and-roles-management.component.ts
+++ ui/src/app/shared/components/shared-privileges-and-roles-management/shared-privileges-and-roles-management.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shared-privileges-and-roles-management',
-  templateUrl: './shared-privileges-and-roles-management.component.html',
-  styleUrls: ['./shared-privileges-and-roles-management.component.scss']
+  selector: "app-shared-privileges-and-roles-management",
+  templateUrl: "./shared-privileges-and-roles-management.component.html",
+  styleUrls: ["./shared-privileges-and-roles-management.component.scss"],
 })
 export class SharedPrivilegesAndRolesManagementComponent implements OnInit {
+  @Input() currentUser: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
