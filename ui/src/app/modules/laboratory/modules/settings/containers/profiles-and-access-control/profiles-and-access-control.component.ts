4150f7c09e4d9cc50c30e432583bb7d331d5be53
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-05-12 14:13:35 +0300

    Add access control to other maintenance modules

--- ui/src/app/modules/laboratory/modules/settings/containers/profiles-and-access-control/profiles-and-access-control.component.ts
+++ ui/src/app/modules/laboratory/modules/settings/containers/profiles-and-access-control/profiles-and-access-control.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-profiles-and-access-control',
-  templateUrl: './profiles-and-access-control.component.html',
-  styleUrls: ['./profiles-and-access-control.component.scss']
+  selector: "app-profiles-and-access-control",
+  templateUrl: "./profiles-and-access-control.component.html",
+  styleUrls: ["./profiles-and-access-control.component.scss"],
 })
 export class ProfilesAndAccessControlComponent implements OnInit {
+  @Input() currentUser: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
