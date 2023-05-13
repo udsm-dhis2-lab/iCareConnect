4831787f29957a862976880353d3bfbf3abf9e18
Dkiba <kibahiladennis@gmail.com>
2023-05-13 15:40:59 +0300

    added audit logs final code

--- omods/core/api/src/test/java/org/openmrs/module/icare/auditlog/BaseAuditLogTest.java
+++ omods/core/api/src/test/java/org/openmrs/module/icare/auditlog/BaseAuditLogTest.java
@@ -20,67 +20,67 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 public class BaseAuditLogTest extends BaseModuleContextSensitiveTest {
-
-    protected static final String MODULE_TEST_DATA = "audit-log.xml";
-
-    protected AuditLogService auditLogService;
-
-    protected AuditLogHelper helper;
-
-    @Before
-    public void before() throws Exception {
-        auditLogService = Context.getService(AuditLogService.class);
-        helper = Context.getRegisteredComponents(AuditLogHelper.class).get(0);
-        executeDataSet(MODULE_TEST_DATA);
-        String exceptionsGpValue = "org.openmrs.Concept,org.openmrs.EncounterType,org.openmrs.PatientIdentifierType";
-        setAuditConfiguration(AuditStrategy.NONE_EXCEPT, exceptionsGpValue, false);
-
-        ExceptionBasedAuditStrategy strategy = (ExceptionBasedAuditStrategy) auditLogService.getAuditingStrategy();
-        assertEquals(AuditStrategy.NONE_EXCEPT, strategy);
-        Set<Class<?>> exceptions = strategy.getExceptions();
-        System.out.println("size: "+exceptions.getClass().getName());
-        assertEquals(5, exceptions.size());
-        assertTrue(OpenmrsUtil.collectionContains(exceptions, Concept.class));
-        assertTrue(OpenmrsUtil.collectionContains(exceptions, ConceptNumeric.class));
-        assertTrue(OpenmrsUtil.collectionContains(exceptions, ConceptComplex.class));
-        assertTrue(OpenmrsUtil.collectionContains(exceptions, EncounterType.class));
-        assertTrue(OpenmrsUtil.collectionContains(exceptions, PatientIdentifierType.class));
-    }
-
-    protected void setAuditConfiguration(AuditStrategy strategy, String exceptionsString,
-                                         boolean storeLastStateOfDeletedItems) throws Exception {
-
-        AuditLogUtil.setGlobalProperty(AuditLogConstants.GP_AUDITING_STRATEGY, strategy.getClass().getName());
-        assertEquals(strategy, auditLogService.getAuditingStrategy());
-        if (exceptionsString != null) {
-            AuditLogUtil.setGlobalProperty(ExceptionBasedAuditStrategy.GLOBAL_PROPERTY_EXCEPTION, exceptionsString);
-        }
-        String value = storeLastStateOfDeletedItems ? "true" : "false";
-        AuditLogUtil.setGlobalProperty(AuditLogConstants.GP_STORE_LAST_STATE_OF_DELETED_ITEMS, value);
-        if (!ExceptionBasedAuditStrategy.class.isAssignableFrom(strategy.getClass())) {
-            String exceptionsGpValue = Context.getAdministrationService().getGlobalProperty(
-                    ExceptionBasedAuditStrategy.GLOBAL_PROPERTY_EXCEPTION);
-            assertEquals(true, StringUtils.isBlank(exceptionsGpValue));
-        }
-    }
-
-    protected void startAuditing(Class<?> clazz) throws Exception {
-        Set<Class<?>> classes = new HashSet<Class<?>>();
-        classes.add(clazz);
-        startAuditing(classes);
-    }
-
-    protected void startAuditing(Set<Class<?>> classes) throws Exception {
-        ((ConfigurableAuditStrategy) auditLogService.getAuditingStrategy()).startAuditing(classes);
-    }
-
-    protected void stopAuditing(Class<?> clazz) throws Exception {
-        Set<Class<?>> classes = new HashSet<Class<?>>();
-        classes.add(clazz);
-        stopAuditing(classes);
-    }
-
-    protected void stopAuditing(Set<Class<?>> classes) throws Exception {
-        ((ConfigurableAuditStrategy) auditLogService.getAuditingStrategy()).stopAuditing(classes);
-    }
+	
+	protected static final String MODULE_TEST_DATA = "audit-log.xml";
+	
+	protected AuditLogService auditLogService;
+	
+	protected AuditLogHelper helper;
+	
+	@Before
+	public void before() throws Exception {
+		auditLogService = Context.getService(AuditLogService.class);
+		helper = Context.getRegisteredComponents(AuditLogHelper.class).get(0);
+		executeDataSet(MODULE_TEST_DATA);
+		String exceptionsGpValue = "org.openmrs.Concept,org.openmrs.EncounterType,org.openmrs.PatientIdentifierType";
+		setAuditConfiguration(AuditStrategy.NONE_EXCEPT, exceptionsGpValue, false);
+		
+		ExceptionBasedAuditStrategy strategy = (ExceptionBasedAuditStrategy) auditLogService.getAuditingStrategy();
+		assertEquals(AuditStrategy.NONE_EXCEPT, strategy);
+		Set<Class<?>> exceptions = strategy.getExceptions();
+		System.out.println("size: " + exceptions.getClass().getName());
+		assertEquals(5, exceptions.size());
+		assertTrue(OpenmrsUtil.collectionContains(exceptions, Concept.class));
+		assertTrue(OpenmrsUtil.collectionContains(exceptions, ConceptNumeric.class));
+		assertTrue(OpenmrsUtil.collectionContains(exceptions, ConceptComplex.class));
+		assertTrue(OpenmrsUtil.collectionContains(exceptions, EncounterType.class));
+		assertTrue(OpenmrsUtil.collectionContains(exceptions, PatientIdentifierType.class));
+	}
+	
+	protected void setAuditConfiguration(AuditStrategy strategy, String exceptionsString,
+	        boolean storeLastStateOfDeletedItems) throws Exception {
+		
+		AuditLogUtil.setGlobalProperty(AuditLogConstants.GP_AUDITING_STRATEGY, strategy.getClass().getName());
+		assertEquals(strategy, auditLogService.getAuditingStrategy());
+		if (exceptionsString != null) {
+			AuditLogUtil.setGlobalProperty(ExceptionBasedAuditStrategy.GLOBAL_PROPERTY_EXCEPTION, exceptionsString);
+		}
+		String value = storeLastStateOfDeletedItems ? "true" : "false";
+		AuditLogUtil.setGlobalProperty(AuditLogConstants.GP_STORE_LAST_STATE_OF_DELETED_ITEMS, value);
+		if (!ExceptionBasedAuditStrategy.class.isAssignableFrom(strategy.getClass())) {
+			String exceptionsGpValue = Context.getAdministrationService().getGlobalProperty(
+			    ExceptionBasedAuditStrategy.GLOBAL_PROPERTY_EXCEPTION);
+			assertEquals(true, StringUtils.isBlank(exceptionsGpValue));
+		}
+	}
+	
+	protected void startAuditing(Class<?> clazz) throws Exception {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(clazz);
+		startAuditing(classes);
+	}
+	
+	protected void startAuditing(Set<Class<?>> classes) throws Exception {
+		((ConfigurableAuditStrategy) auditLogService.getAuditingStrategy()).startAuditing(classes);
+	}
+	
+	protected void stopAuditing(Class<?> clazz) throws Exception {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(clazz);
+		stopAuditing(classes);
+	}
+	
+	protected void stopAuditing(Set<Class<?>> classes) throws Exception {
+		((ConfigurableAuditStrategy) auditLogService.getAuditingStrategy()).stopAuditing(classes);
+	}
 }
--- omods/core/api/src/test/java/org/openmrs/module/icare/auditlog/BaseBehaviorTest.java
+++ omods/core/api/src/test/java/org/openmrs/module/icare/auditlog/BaseBehaviorTest.java
@@ -8,35 +8,35 @@ import org.openmrs.api.context.Context;
 import java.io.Serializable;
 import java.util.List;
 
-public class BaseBehaviorTest extends BaseAuditLogTest{
-
-    protected ConceptService conceptService;
-
-    protected EncounterService encounterService;
-
-    @Before
-    public void setup() throws Exception {
-        conceptService = Context.getConceptService();
-        encounterService = Context.getEncounterService();
-        //No log entries should be existing
-        Assert.assertTrue(getAllLogs().isEmpty());
-    }
-
-    /**
-     * Utility method to get all logs
-     *
-     * @return a list of {@link AuditLog}s
-     */
-    protected List<AuditLog> getAllLogs() {
-        return auditLogService.getAuditLogs(null, null, null, null, false, null, null);
-    }
-
-    /**
-     * Utility method to get all logs for a specific object
-     *
-     * @return a list of {@link AuditLog}s
-     */
-    protected List<AuditLog> getAllLogs(Serializable id, Class<?> clazz, List<AuditLog.Action> actions) {
-        return auditLogService.getAuditLogs(id, clazz, actions, null, null, false);
-    }
+public class BaseBehaviorTest extends BaseAuditLogTest {
+	
+	protected ConceptService conceptService;
+	
+	protected EncounterService encounterService;
+	
+	@Before
+	public void setup() throws Exception {
+		conceptService = Context.getConceptService();
+		encounterService = Context.getEncounterService();
+		//No log entries should be existing
+		Assert.assertTrue(getAllLogs().isEmpty());
+	}
+	
+	/**
+	 * Utility method to get all logs
+	 * 
+	 * @return a list of {@link AuditLog}s
+	 */
+	protected List<AuditLog> getAllLogs() {
+		return auditLogService.getAuditLogs(null, null, null, null, false, null, null);
+	}
+	
+	/**
+	 * Utility method to get all logs for a specific object
+	 * 
+	 * @return a list of {@link AuditLog}s
+	 */
+	protected List<AuditLog> getAllLogs(Serializable id, Class<?> clazz, List<AuditLog.Action> actions) {
+		return auditLogService.getAuditLogs(id, clazz, actions, null, null, false);
+	}
 }
