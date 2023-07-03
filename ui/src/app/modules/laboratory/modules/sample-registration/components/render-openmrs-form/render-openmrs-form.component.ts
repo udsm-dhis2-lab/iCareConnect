0489789b046d7eae04fa0f2505502b1fd7f1286a
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-07-03 13:33:45 +0300

    Add support to render custom form

--- ui/src/app/modules/laboratory/modules/sample-registration/components/render-openmrs-form/render-openmrs-form.component.ts
+++ ui/src/app/modules/laboratory/modules/sample-registration/components/render-openmrs-form/render-openmrs-form.component.ts
@@ -1,26 +1,22 @@
 import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { loadCustomOpenMRSForms } from "src/app/store/actions";
 import { AppState } from "src/app/store/reducers";
 import { getCustomOpenMRSFormById } from "src/app/store/selectors/form.selectors";
 
 @Component({
-  selector: "app-dynamic-openmrs-registration-form",
-  templateUrl: "./dynamic-openmrs-registration-form.component.html",
-  styleUrls: ["./dynamic-openmrs-registration-form.component.scss"],
+  selector: "app-render-openmrs-form",
+  templateUrl: "./render-openmrs-form.component.html",
+  styleUrls: ["./render-openmrs-form.component.scss"],
 })
-export class DynamicOpenmrsRegistrationFormComponent implements OnInit {
-  @Input() formUuids: string[];
+export class RenderOpenmrsFormComponent implements OnInit {
+  @Input() formId: string;
+  customForm$: Observable<any>;
   @Output() formUpdate: EventEmitter<any> = new EventEmitter<any>();
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store.dispatch(
-      loadCustomOpenMRSForms({
-        formUuids: this.formUuids,
-      })
-    );
+    this.customForm$ = this.store.select(getCustomOpenMRSFormById(this.formId));
   }
 
   onFormUpdate(data: any): void {
