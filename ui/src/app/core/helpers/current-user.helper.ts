bdb8f279319adaecec6306102922309b73f34700
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-07-09 15:26:02 +0300

    Add support to control showing patient names using sharing settings

--- ui/src/app/core/helpers/current-user.helper.ts
+++ ui/src/app/core/helpers/current-user.helper.ts
@@ -1,7 +1,6 @@
 import { UserGet } from "src/app/shared/resources/openmrs";
 
 export function formatCurrentUserDetails(userDetails): UserGet {
-  console.log("userDetails", userDetails);
   return {
     links: userDetails.links,
     uuid: userDetails.uuid,
