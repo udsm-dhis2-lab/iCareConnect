33694533369a9738d86d69ea131b36d05a4500bf
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-07-31 12:40:37 +0300

    Add support to validate date and datetime form fields

--- ui/src/app/shared/modules/form/helpers/validate-form-fields.helper.ts
+++ ui/src/app/shared/modules/form/helpers/validate-form-fields.helper.ts
@@ -5,15 +5,70 @@ export function validateFormFields(rules: any[], values: any): any {
   let validationIssues = [];
   rules?.forEach((rule) => {
     if (rule) {
-      validationIssues = [
-        ...validationIssues,
-        {
-          message: rule?.message,
-          action: rule?.action,
-          field: rule?.field?.concept,
-        },
-      ];
+      // console.log("match(/#{.+?}/g)", rule?.condition?.match(/#{.+?}/g));
+      rule?.condition?.match(/#{.+?}/g);
+      if (
+        rule?.condition?.indexOf("=") === -1 &&
+        rule?.condition?.indexOf(">") > -1
+      ) {
+        const dataType = rule?.field?.dataType;
+        const violated = evaluateLeftLessThanRight(
+          rule?.condition,
+          values,
+          dataType
+        );
+        validationIssues = violated
+          ? [
+              ...validationIssues,
+              {
+                message: rule?.message,
+                action: rule?.action,
+                field: rule?.field?.concept,
+              },
+            ]
+          : validationIssues;
+      }
     }
   });
   return keyBy(validationIssues, "field");
+}
+
+function evaluateLeftLessThanRight(
+  expression: string,
+  values: any,
+  dataType?: string
+): boolean {
+  let leftExpression = expression?.split(">")[0];
+  let rightExpression = expression?.split(">")[1];
+
+  const leftSideReferences = leftExpression.match(/#{.+?}/g);
+  const rightSideReferences = rightExpression.match(/#{.+?}/g);
+
+  leftSideReferences?.forEach((reference) => {
+    const referenceUuid = reference?.replace("#{", "").replace("}", "");
+    const val = values[referenceUuid] ? values[referenceUuid] : 0;
+    leftExpression = leftExpression.replace(reference, val);
+  });
+
+  const leftSideValue =
+    leftSideReferences?.length > 1 ? eval(leftExpression) : leftExpression;
+
+  rightSideReferences?.forEach((reference) => {
+    const referenceUuid = reference?.replace("#{", "").replace("}", "");
+    const val = values[referenceUuid] ? values[referenceUuid] : 0;
+    rightExpression = rightExpression.replace(reference, val);
+  });
+
+  const rightSideValue =
+    rightSideReferences?.length > 1 ? eval(rightExpression) : rightExpression;
+
+  if (
+    (dataType === "DATE" || dataType === "DATETIME") &&
+    new Date(leftSideValue) < new Date(rightSideValue)
+  ) {
+    return true;
+  } else if (leftSideValue < rightSideValue) {
+  } else {
+    return false;
+  }
 }
