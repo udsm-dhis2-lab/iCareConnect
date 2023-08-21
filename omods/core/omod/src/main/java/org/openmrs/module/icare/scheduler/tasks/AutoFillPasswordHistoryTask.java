3d796abc1bbd067977eeee9772e22874b96afcad
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-08-21 11:52:22 +0300

    Add filter to un-voided orders on allocations by sample API

--- omods/core/omod/src/main/java/org/openmrs/module/icare/scheduler/tasks/AutoFillPasswordHistoryTask.java
+++ omods/core/omod/src/main/java/org/openmrs/module/icare/scheduler/tasks/AutoFillPasswordHistoryTask.java
@@ -7,33 +7,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutoFillPasswordHistoryTask extends AbstractTask {
-
-    private static final Logger log = LoggerFactory.getLogger(AutoFillPasswordHistoryTask.class);
-
-
-    public AutoFillPasswordHistoryTask(){}
-    @Override
-    public void execute() {
-
-        if (!this.isExecuting) {
-            if (log.isDebugEnabled()) {
-                log.debug("Starting Auto Fill Password History Task...");
-            }
-
-            this.startExecuting();
-
-            try {
-                ICareService iCareService = Context.getService(ICareService.class);
-                iCareService.updatePasswordHistory();
-
-            }
-            catch (Exception var5) {
-                log.error("Error while auto fill password ", var5);
-            }
-            finally {
-                this.stopExecuting();
-            }
-        }
-    }
-
+	
+	private static final Logger log = LoggerFactory.getLogger(AutoFillPasswordHistoryTask.class);
+	
+	public AutoFillPasswordHistoryTask() {
+	}
+	
+	@Override
+	public void execute() {
+		
+		if (!this.isExecuting) {
+			if (log.isDebugEnabled()) {
+				log.debug("Starting Auto Fill Password History Task...");
+			}
+			
+			this.startExecuting();
+			
+			try {
+				ICareService iCareService = Context.getService(ICareService.class);
+				iCareService.updatePasswordHistory();
+				
+			}
+			catch (Exception var5) {
+				log.error("Error while auto fill password ", var5);
+			}
+			finally {
+				this.stopExecuting();
+			}
+		}
+	}
+	
 }
