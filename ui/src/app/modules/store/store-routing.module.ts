1b5ed92439f31de1227ab59d6c52d7660a1625b4
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-09-05 20:33:51 +0300

    Refector store components to be sharable between modules

--- ui/src/app/modules/store/store-routing.module.ts
+++ ui/src/app/modules/store/store-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ExpiredItemsComponent } from "./components/expired-items/expired-items.component";
-import { NearlyExpiredItemsComponent } from "./components/nearly-expired/nearly-expired-items.component";
-import { NearlyStockedOutItemsComponent } from "./components/nearly-stocked-out/nearly-stocked-out-items.component";
-import { StockOutItemsComponent } from "./components/stock-out-items/stock-out-items.component";
-import { IssuingPageComponent } from "./pages/issuing-page/issuing-page.component";
-import { IssuingComponent } from "./pages/issuing/issuing.component";
-import { RequisitionPageComponent } from "./pages/requisition-page/requisition-page.component";
-import { RequisitionReceiptComponent } from "./pages/requisition-receipt/requisition-receipt.component";
-import { RequisitionComponent } from "./pages/requisition/requisition.component";
-import { StockComponent } from "./pages/stock/stock.component";
-import { StoreHomeComponent } from "./pages/store-home/store-home.component";
-import { StoreSettingsComponent } from "./pages/store-settings/store-settings.component";
-import { StoreTransactionComponent } from "./pages/store-transaction/store-transaction.component";
+import { ExpiredItemsComponent } from "../../shared/store-components/expired-items/expired-items.component";
+import { StoreHomeComponent } from "src/app/shared/store-pages/store-home/store-home.component";
+import { StockComponent } from "src/app/shared/store-pages/stock/stock.component";
+import { RequisitionPageComponent } from "src/app/shared/store-pages/requisition-page/requisition-page.component";
+import { IssuingPageComponent } from "src/app/shared/store-pages/issuing-page/issuing-page.component";
+import { StoreTransactionComponent } from "src/app/shared/store-pages/store-transaction/store-transaction.component";
+import { StockOutItemsComponent } from "src/app/shared/store-components/stock-out-items/stock-out-items.component";
+import { StoreSettingsComponent } from "src/app/shared/store-pages/store-settings/store-settings.component";
+import { NearlyStockedOutItemsComponent } from "src/app/shared/store-components/nearly-stocked-out/nearly-stocked-out-items.component";
+import { NearlyExpiredItemsComponent } from "src/app/shared/store-components/nearly-expired/nearly-expired-items.component";
 
 const routes: Routes = [
   {
