39957093d47ae6bcea894a4ac0158ca76061b238
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-09-05 20:33:51 +0300

    Refector store components to be sharable between modules

--- ui/src/app/modules/pharmacy/pharmacy-routing.module.ts
+++ ui/src/app/modules/pharmacy/pharmacy-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PharmacyComponent } from "./containers/pharmacy/pharmacy.component";
+import { StockComponent } from "src/app/shared/store-pages/stock/stock.component";
+import { RequisitionPageComponent } from "src/app/shared/store-pages/requisition-page/requisition-page.component";
+import { IssuingPageComponent } from "src/app/shared/store-pages/issuing-page/issuing-page.component";
+import { StoreSettingsComponent } from "src/app/shared/store-pages/store-settings/store-settings.component";
 const routes: Routes = [
   {
     path: "",
@@ -19,6 +23,22 @@ const routes: Routes = [
           import("./modules/cashier/cashier.module").then(
             (m) => m.CashierModule
           ),
+      },
+      {
+        path: "stock",
+        component: StockComponent,
+      },
+      {
+        path: "requisition",
+        component: RequisitionPageComponent,
+      },
+      {
+        path: "issuing",
+        component: IssuingPageComponent,
+      },
+      {
+        path: "settings",
+        component: StoreSettingsComponent,
       },
     ],
   },
---
+++ ui/src/app/shared/store-pages/requisition-page/requisition-page.component.ts
@@ -0,0 +1,21 @@
+import { ThrowStmt } from "@angular/compiler";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { AppState } from "src/app/store/reducers";
+import { getCurrentLocation } from "src/app/store/selectors";
+
+@Component({
+  selector: "app-requisition-page",
+  templateUrl: "./requisition-page.component.html",
+  styleUrls: ["./requisition-page.component.scss"],
+})
+export class RequisitionPageComponent implements OnInit {
+  currentStore$: Observable<any>;
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.currentStore$ = this.store.pipe(select(getCurrentLocation(false)));
+  }
+}
