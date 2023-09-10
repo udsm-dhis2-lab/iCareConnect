946beedb60767ed8289571b34e61c58352ea0216
Marco Masembo <marcomasembo1@gmail.com>
2023-09-10 13:30:09 +0300

    adding feature to print issued and requested items

--- ui/src/app/modules/store/pages/issuing/issuing.component.ts
+++ ui/src/app/modules/store/pages/issuing/issuing.component.ts
@@ -1,30 +1,34 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { MatSelectChange } from "@angular/material/select";
 import { select, Store } from "@ngrx/store";
 import { IssuingObject } from "src/app/shared/resources/store/models/issuing.model";
 import { IssuingService } from "src/app/shared/resources/store/services/issuing.service";
 import { orderBy, flatten, omit } from "lodash";
-import { Observable, zip } from "rxjs";
+import { Observable, zip, Subscription } from "rxjs";
 import { map, tap } from "rxjs/operators";
 import { LocationGet } from "src/app/shared/resources/openmrs";
 import { loadLocationsByTagName } from "src/app/store/actions";
 import { AppState } from "src/app/store/reducers";
-import { getStoreLocations } from "src/app/store/selectors";
+import { getParentLocation, getStoreLocations } from "src/app/store/selectors";
 import { ConfirmRequisitionsModalComponent } from "../../modals/confirm-requisitions-modal/confirm-requisitions-modal.component";
 import { IssuingFormComponent } from "../../modals/issuing-form/issuing-form.component";
 import { RequestCancelComponent } from "../../modals/request-cancel/request-cancel.component";
 import { RequisitionService } from "src/app/shared/resources/store/services/requisition.service";
 import { SharedConfirmationComponent } from "src/app/shared/components/shared-confirmation/shared-confirmation.component";
+import { getFilteredIssueItems } from "src/app/shared/helpers/issuings.helper";
+import { getCurrentUserDetails } from "src/app/store/selectors/current-user.selectors";
+import { formatDateToString } from "src/app/shared/helpers/format-date.helper";
 
 @Component({
   selector: "app-issuing",
   templateUrl: "./issuing.component.html",
   styleUrls: ["./issuing.component.scss"],
 })
-export class IssuingComponent implements OnInit {
+export class IssuingComponent implements OnInit, OnDestroy {
   @Input() currentLocation: any;
 
+  subscription: Subscription;
   issuingList$: Observable<IssuingObject[]>;
   loadingIssuingList$: Observable<boolean>;
   currentStore$: Observable<LocationGet>;
@@ -43,6 +47,9 @@ export class IssuingComponent implements OnInit {
   q: string;
   startDate: Date;
   endDate: Date;
+  specificRequisition$: Observable<any>;
+  facilityDetails$: Observable<any>;
+  currentUser$: Observable<any>;
 
   constructor(
     private store: Store<AppState>,
@@ -61,8 +68,12 @@ export class IssuingComponent implements OnInit {
     this.getAllIssuing();
     // this.loadingIssuingList$ = this.store.pipe(select(getIssuingLoadingState));
     this.stores$ = this.store.pipe(select(getStoreLocations));
+    this.facilityDetails$ = this.store.select(getParentLocation);
+    this.currentUser$ = this.store.select(getCurrentUserDetails);
   }
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
   onGetSearchingText(q: string): void {
     this.q = q;
     this.getAllIssuing();
@@ -134,7 +145,7 @@ export class IssuingComponent implements OnInit {
   }
 
   getAllIssuing(): void {
-    this.loadingIssues = true
+    this.loadingIssues = true;
     this.issuingList$ = this.issuingService
       .getIssuings(
         this.currentLocation?.id,
@@ -144,10 +155,10 @@ export class IssuingComponent implements OnInit {
         this.selectedStatus,
         "DESC",
         {
-         q: this.q,
-         startDate: this.startDate,
-         endDate: this.endDate,
-       }
+          q: this.q,
+          startDate: this.startDate,
+          endDate: this.endDate,
+        }
       )
       ?.pipe(
         map((response) => {
@@ -176,9 +187,9 @@ export class IssuingComponent implements OnInit {
             requisitionItemStatuses: [
               {
                 status: "REJECTED",
-                remarks: result?.reason
-              }
-            ]
+                remarks: result?.reason,
+              },
+            ],
           };
 
           this.requisitionService
@@ -215,8 +226,8 @@ export class IssuingComponent implements OnInit {
         width: "20%",
         data: {
           items: itemsToIssue,
-          issue: issue
-        }
+          issue: issue,
+        },
       })
       .afterClosed()
       .subscribe((requisitionData) => {
@@ -232,34 +243,36 @@ export class IssuingComponent implements OnInit {
         if (items) {
           zip(
             ...items.map((item) => {
-                // Determine all batches eligible to match the requested quantity
-                const batchesForThisItem = nonExpiredBatches.filter((batch) => batch?.item?.uuid === item?.item?.uuid) || [];
-                let eligibleBatches = [];
-                const quantityToIssue = Number(item?.quantity);
-                let eligibleToIssue = 0;
-                let count = 0;
-                while (
-                  batchesForThisItem?.length > 0 &&
-                  quantityToIssue > eligibleToIssue
-                ) {
-                  const toIssue =
-                    quantityToIssue - eligibleToIssue >
-                    Number(batchesForThisItem[count]?.quantity)
-                      ? Number(batchesForThisItem[count]?.quantity)
-                      : quantityToIssue - eligibleToIssue;
-                  eligibleBatches = [
-                    ...eligibleBatches,
-                    {
-                      ...batchesForThisItem[count],
-                      ...item,
-                      toIssue: toIssue,
-                    },
-                  ];
-                  eligibleToIssue =
-                    eligibleToIssue +
-                    Number(batchesForThisItem[count]?.quantity);
-                  count++;
-                }
+              // Determine all batches eligible to match the requested quantity
+              const batchesForThisItem =
+                nonExpiredBatches.filter(
+                  (batch) => batch?.item?.uuid === item?.item?.uuid
+                ) || [];
+              let eligibleBatches = [];
+              const quantityToIssue = Number(item?.quantity);
+              let eligibleToIssue = 0;
+              let count = 0;
+              while (
+                batchesForThisItem?.length > 0 &&
+                quantityToIssue > eligibleToIssue
+              ) {
+                const toIssue =
+                  quantityToIssue - eligibleToIssue >
+                  Number(batchesForThisItem[count]?.quantity)
+                    ? Number(batchesForThisItem[count]?.quantity)
+                    : quantityToIssue - eligibleToIssue;
+                eligibleBatches = [
+                  ...eligibleBatches,
+                  {
+                    ...batchesForThisItem[count],
+                    ...item,
+                    toIssue: toIssue,
+                  },
+                ];
+                eligibleToIssue =
+                  eligibleToIssue + Number(batchesForThisItem[count]?.quantity);
+                count++;
+              }
               const issueInput = {
                 requisitionUuid: issue?.uuid,
                 issuedLocationUuid: issue?.requestingLocation.uuid,
@@ -281,7 +294,7 @@ export class IssuingComponent implements OnInit {
             })
           ).subscribe((response: any) => {
             if (response) {
-              if(!response?.error){
+              if (!response?.error) {
                 this.selectedItems = {};
                 this.getAllIssuing();
               }
@@ -290,7 +303,7 @@ export class IssuingComponent implements OnInit {
         }
       });
   }
-  
+
   rejectAllSelected(event: Event, issue: any): void {
     event.stopPropagation();
     let itemsToReject = [];
@@ -318,24 +331,25 @@ export class IssuingComponent implements OnInit {
                   requisitionItemStatuses: [
                     {
                       status: "REJECTED",
-                      remarks: results?.reason
-                    }
-                  ]
-                }
-                return this.requisitionService
-                  .updateRequisitionItem(item?.uuid, ItemObject)
+                      remarks: results?.reason,
+                    },
+                  ],
+                };
+                return this.requisitionService.updateRequisitionItem(
+                  item?.uuid,
+                  ItemObject
+                );
               })
             ).subscribe((response: any) => {
               if (response) {
-                if(!response?.error){
+                if (!response?.error) {
                   this.selectedItems = {};
                   this.getAllIssuing();
                 }
               }
             });
           }
-        } 
-
+        }
       });
   }
 
@@ -361,6 +375,259 @@ export class IssuingComponent implements OnInit {
     } else {
       this.viewIssueItems = issueUuid;
     }
+  }
+
+  onPrint(e: Event, issue: any, facilityDetails: any, currentUser: any) {
+    let itemsToPrint: any;
+    // this.specificRequisition$ =
+    this.subscription = this.requisitionService
+      .getRequisitionByUuid(issue?.uuid)
+      .pipe(
+        map((response) => {
+          const items = getFilteredIssueItems(
+            response?.requisitionItems,
+            response?.issues
+          );
+          return { ...response, items: items };
+        })
+      )
+      .subscribe((response) => {
+        itemsToPrint = response.items;
+
+        console.log("Items", itemsToPrint);
+        let contents: string;
+        let printingDate = formatDateToString(new Date());
+        let issueDate = formatDateToString(new Date(issue?.created));
+
+        const frame1: any = document.createElement("iframe");
+        frame1.name = "frame3";
+        frame1.style.position = "absolute";
+        frame1.style.width = "100%";
+        frame1.style.top = "-1000000px";
+        document.body.appendChild(frame1);
+
+        var frameDoc = frame1.contentWindow
+          ? frame1.contentWindow
+          : frame1.contentDocument.document
+          ? frame1.contentDocument.document
+          : frame1.contentDocument;
+
+        frameDoc.document.open();
+
+        frameDoc.document.write(`
+         <html>
+          <head>
+            <style>
+                @page { size: auto;  margin: 0mm; }
+
+                body {
+                  padding: 30px;
+                  margin: 0 auto;
+                  width: 150mm;
+                }
+
+                #top .logo img{
+                  //float: left;
+                  height: 100px;
+                  width: 100px;
+                  background-size: 100px 100px;
+                }
+                .info h2 {
+                  font-size: 1.3em;
+                }
+                h3 {
+                  font-size: 1em;
+                }
+                h5 {
+                  font-size: .7em;
+                }
+                p {
+                  font-size: .7em;
+                }
+                #table {
+                  font-family: Arial, Helvetica, sans-serif;
+                  border-collapse: collapse;
+                  width: 100%;
+                  background-color: #000;
+                }
+                #table td, #table  th {
+                  border: 1px solid #ddd;
+                  padding: 5px;
+                }
+
+                #table tbody tr:nth-child(even){
+                  background-color: #f2f2f2;
+                }
+
+                #table thead tr th {
+                  padding-top:6px;
+                  padding-bottom: 6px;
+                  text-align: left;
+                  background-color: #cecece;
+                  font-size: .7em;
+                }
+                tbody tr td {
+                  font-size: .7em;
+                }
+                .footer {
+                  margin-top:50px
+                }
+                .footer .userDetails .signature {
+                  margin-top: 20px;
+                }
+            </style>
+          </head>
+          <body>
+           <div id="printOut">
+          `);
+
+        // Change image from base64 then replace some text with empty string to get an image
+
+        let image = "";
+        let header = "";
+        let subHeader = "";
+
+        facilityDetails.attributes.map((attribute) => {
+          let attributeTypeName =
+            attribute && attribute.attributeType
+              ? attribute?.attributeType?.name.toLowerCase()
+              : "";
+          if (attributeTypeName === "logo") {
+            image = attribute?.value;
+          }
+          header = attributeTypeName === "header" ? attribute?.value : "";
+          subHeader =
+            attributeTypeName === "sub header" ? attribute?.value : "";
+        });
+
+        frameDoc.document.write(`
+
+        <center id="top">
+           <div class="info">
+            <h2>${header.length > 0 ? header : facilityDetails.display} </h2>
+            </div>
+          <div class="logo">
+            <img src="${image}" alt="Facility's Logo">
+          </div>
+
+          <div class="info">
+            <h2>${
+              subHeader.length > 0 ? subHeader : facilityDetails.description
+            } </h2>
+            <h3>P.O Box ${facilityDetails.postalCode} ${
+          facilityDetails.stateProvince
+        }</h3>
+            <h3>${facilityDetails.country}</h3>
+          </div>
+          <!--End Info-->
+        </center>
+        <!--End Document top-->
+
+        <div id="mid">
+          <div class="patient-info">
+            <p>
+                <b>Issue Number:</b>  ${issue.code}</br>
+            </p>
+            <p>
+                <b>Issuig Store:</b> ${this.currentLocation?.display}</br>
+            </p>
+            <p>
+               <b> Requesting Store:</b> ${
+                 issue?.requestingLocation?.display
+               }</br>
+            </p>
+            <p>
+                <b>Requested on:</b> ${issueDate}</br>
+            </p>
+            <p>
+                <b>Status:</b> ${
+                  issue?.requisitionStatuses[
+                    issue?.requisitionStatuses?.length - 1
+                  ]?.status
+                    ? issue?.requisitionStatuses[
+                        issue?.requisitionStatuses?.length - 1
+                      ]?.status
+                    : "DRAFT"
+                }</br>
+                  
+            </p>
+          </div>
+        </div>`);
+
+        //For paid items
+        if (itemsToPrint.length > 0) {
+          let title = "items";
+          frameDoc.document.write(`
+          <div>
+            <h5>${title}</h5>
+          </div>
+          <table id="table">
+            <thead>
+              <tr>
+                <th>SN</th>
+                <th>Item</th>
+                <th>Quantity</th>
+                <th>Batch</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>`);
+
+          itemsToPrint.forEach((stock, index) => {
+            let currentIndex = index + 1;
+            // let paymentDate = new Date(payment.created);
+            // Date to string
+            // let date_paid = formatDateToString(paymentDate);
+
+            // let date_paid = `${
+            //   paymentDate.getDate().toString().length > 1
+            //     ? paymentDate.getDate()
+            //     : "0" + paymentDate.getDate()
+            // }-${
+            //   paymentDate.getMonth().toString().length > 1
+            //     ? paymentDate.getMonth() + 1
+            //     : "0" + paymentDate.getMonth() + 1
+            // }-${paymentDate.getFullYear()}`;
+            contents = `
+                  <tr>
+                    <td>${currentIndex}</td>
+                    <td>${stock?.item.display}</td>
+                    <td>${stock?.quantity}</td>
+                    <td>${stock?.batch}</td>
+                    <td>${stock?.status}</td>
+                  </tr>`;
+
+            frameDoc.document.write(contents);
+          });
+
+          frameDoc.document.write(`
+            </tbody>
+          </table>`);
+        }
+
+        frameDoc.document.write(`
+            <div class="footer">
+              <div class="userDetails">
+                <p class="name">Printed By: ${currentUser?.person?.display}</p>
+                <p class="signature">Signature : ..............................</p>
+              </div>
+
+              <div class=""printDate>
+                <p>Printed on: ${printingDate}</p>
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>`);
+
+        frameDoc.document.close();
+
+        setTimeout(function () {
+          window.frames["frame3"].focus();
+          window.frames["frame3"].print();
+          document.body.removeChild(frame1);
+        }, 500);
+      });
   }
 
   // onIssue(e: any, issue?: IssuingObject, currentStore?: LocationGet): void {
