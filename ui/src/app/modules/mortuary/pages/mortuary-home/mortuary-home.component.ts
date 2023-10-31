d93286604cce13df2b665c25d4ae23235fa1fdc8
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-10-31 14:34:09 +0300

    Add support to load death registry encounter type uuid

--- ui/src/app/modules/mortuary/pages/mortuary-home/mortuary-home.component.ts
+++ ui/src/app/modules/mortuary/pages/mortuary-home/mortuary-home.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { select, Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { take } from "rxjs/operators";
+import { take, tap } from "rxjs/operators";
 import { Location } from "src/app/core/models";
+import { SystemSettingsService } from "src/app/core/services/system-settings.service";
 import { VisitObject } from "src/app/shared/resources/visits/models/visit-object.model";
 import {
   go,
@@ -32,8 +33,14 @@ export class MortuaryHomeComponent implements OnInit {
   loadingVisit$: Observable<boolean>;
   cabinetsUnderCurrentLocation$: Observable<Location[]>;
   currentLocation$: Observable<Location>;
+  deathRegistryEncounterTypeUuid$: Observable<string>;
+  errors: any[] = [];
 
-  constructor(private store: Store<AppState>, private dialog: MatDialog) {}
+  constructor(
+    private store: Store<AppState>,
+    private dialog: MatDialog,
+    private systemSettingsService: SystemSettingsService
+  ) {}
 
   ngOnInit(): void {
     this.store
@@ -57,9 +64,26 @@ export class MortuaryHomeComponent implements OnInit {
         );
       });
     this.loadingVisit$ = this.store.pipe(select(getVisitLoadingState));
+    this.deathRegistryEncounterTypeUuid$ = this.systemSettingsService
+      .getSystemSettingsByKey(`iCare.mortuary.settings.encounterTypeUuid`)
+      .pipe(
+        tap((response: any) => {
+          this.errors = [
+            ...this.errors,
+            {
+              error: {
+                message:
+                  "iCare.mortuary.settings.encounterTypeUuid is not set, contact IT",
+              },
+            },
+          ];
+          return response;
+        })
+      );
   }
 
-  onSelectPatient() {
+  onSelectPatient(event: any): void {
+    console.log("EVENT", event);
     this.store.dispatch(go({ path: ["/mortuary/dashboard"] }));
   }
 }
