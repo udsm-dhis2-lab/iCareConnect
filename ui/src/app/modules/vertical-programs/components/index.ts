7b6032ee306537a0e97e7deda1fc5bf6b3d3f6d1
Josephat Mwakyusa <josephatjulius24@gmail.com>
2023-11-01 17:28:30 +0300

    Add support to load workflow state forms from system settings

--- ui/src/app/modules/vertical-programs/components/index.ts
+++ ui/src/app/modules/vertical-programs/components/index.ts
@@ -5,6 +5,8 @@ import { EnrolledPatientsListComponent } from "./enrolled-patients-list/enrolled
 import { InvestigationProcedureComponent } from "./investigation-procedure/investigation-procedure.component";
 import { OrdersOptionsFormComponent } from "./orders-options-form/orders-options-form.component";
 import { PrescriptionComponent } from "./prescription/prescription.component";
+import { WorkflowStateFormDataComponent } from "./workflow-state-form-data/workflow-state-form-data.component";
+import { WorkflowStateComponent } from "./workflow-state/workflow-state.component";
 
 export const clinicComponents: any[] = [
   ActiveDiagnosesComponent,
@@ -14,4 +16,6 @@ export const clinicComponents: any[] = [
   DiagnosisComponent,
   InvestigationProcedureComponent,
   EnrolledPatientsListComponent,
+  WorkflowStateComponent,
+  WorkflowStateFormDataComponent,
 ];
