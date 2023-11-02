78a11f17cd93a6d49ca5b74b161cad8f30cdc550
Marco Masembo <marcomasembo1@gmail.com>
2023-11-02 14:24:44 +0300

    enrolling new patient in vertical program

--- ui/src/app/modules/vertical-programs/pages/index.ts
+++ ui/src/app/modules/vertical-programs/pages/index.ts
@@ -1,12 +1,13 @@
-
-import { PatientConsultationComponent } from './patient-consultation/patient-consultation.component';
-import { PatientDashboardComponent } from './patient-dashboard/patient-dashboard.component';
-import { PatientHomeComponent } from './patient-home/patient-home.component';
-import { VerticalProgramsPatientListComponent } from './vertical-program-patient-list/vertical-program-patient-list.component';
+import { PatientConsultationComponent } from "./patient-consultation/patient-consultation.component";
+import { PatientDashboardComponent } from "./patient-dashboard/patient-dashboard.component";
+import { PatientHomeComponent } from "./patient-home/patient-home.component";
+import { VerticalProgramNewEnrollComponent } from "./vertical-program-new-enroll/vertical-program-new-enroll.component";
+import { VerticalProgramsPatientListComponent } from "./vertical-program-patient-list/vertical-program-patient-list.component";
 
 export const clinicPages: any[] = [
   PatientHomeComponent,
   PatientDashboardComponent,
   PatientConsultationComponent,
   VerticalProgramsPatientListComponent,
+  VerticalProgramNewEnrollComponent,
 ];
