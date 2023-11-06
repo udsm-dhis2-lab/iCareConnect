779b600c66886e469b0e7ae296a2ffeed921a50f
Dkiba <kibahiladennis@gmail.com>
2023-11-06 18:44:29 +0300

    add support to discharge a patient in mortuary and add encounter program get api

--- omods/core/api/src/main/java/org/openmrs/module/icare/core/dao/EncounterPatientProgramDAO.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/core/dao/EncounterPatientProgramDAO.java
@@ -1,5 +1,19 @@
 package org.openmrs.module.icare.core.dao;
 
+import org.hibernate.Query;
+import org.openmrs.Encounter;
+import org.openmrs.api.db.hibernate.DbSession;
 import org.openmrs.module.icare.core.models.EncounterPatientProgram;
 
-public class EncounterPatientProgramDAO extends BaseDAO<EncounterPatientProgram> {}
+import java.util.List;
+
+public class EncounterPatientProgramDAO extends BaseDAO<EncounterPatientProgram> {
+    public List<Encounter> getEncounterByPatientProgram(String patientProgramUuid) {
+        DbSession session = this.getSession();
+        String queryStr = " SELECT ep.encounter FROM EncounterProgram ep WHERE ep.patientProgram.uuid =:patientStateUuid";
+        Query query = session.createQuery(queryStr);
+        query.setParameter("patientProgramUuid", patientProgramUuid);
+        return query.list();
+
+    }
+}
