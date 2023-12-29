a942b9dc3059063cdcac2f4f6c2c781b2b0c395a
machakelvin <machakelvin76@gmail.com>
2023-12-29 08:05:29 +0300

    i made it

--- ui/src/app/modules/laboratory/modals/index.ts
+++ ui/src/app/modules/laboratory/modals/index.ts
@@ -1,3 +1,4 @@
 import { AdditionalFieldsModalComponent } from "./additional-fields-modal/additional-fields-modal.component";
 
+
 export const sharedModals: Array<any> = [AdditionalFieldsModalComponent];
