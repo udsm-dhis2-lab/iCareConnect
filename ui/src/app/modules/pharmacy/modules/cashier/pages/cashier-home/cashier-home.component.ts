9ca3c7d2e2a7a6d45cde1da0fc95a9a4911ac8e6
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-01-10 16:42:29 +0300

    Add support to capture dose variables

--- ui/src/app/modules/pharmacy/modules/cashier/pages/cashier-home/cashier-home.component.ts
+++ ui/src/app/modules/pharmacy/modules/cashier/pages/cashier-home/cashier-home.component.ts
@@ -3,7 +3,10 @@ import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { SystemSettingsService } from "src/app/core/services/system-settings.service";
 import { AppState } from "src/app/store/reducers";
-import { getCurrentUserDetails } from "src/app/store/selectors/current-user.selectors";
+import {
+  getCurrentUserDetails,
+  getProviderDetails,
+} from "src/app/store/selectors/current-user.selectors";
 
 @Component({
   selector: "app-cashier-home",
@@ -16,6 +19,9 @@ export class CashierHomeComponent implements OnInit {
   currentLocation: any;
   visitUuid$: Observable<string>;
   patientUuid$: Observable<string>;
+  encounterTypeUuid$: Observable<string>;
+  provider$: Observable<any>;
+  prescriptionVariables$: Observable<any>;
   constructor(
     private store: Store<AppState>,
     private systemSettingsService: SystemSettingsService
@@ -23,6 +29,7 @@ export class CashierHomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.currentUser$ = this.store.select(getCurrentUserDetails);
+    this.provider$ = this.store.select(getProviderDetails);
     // TODO: Add support to catch errors in case configs miss
     this.pharmacyClientDetailsFormUuid$ =
       this.systemSettingsService.getSystemSettingsByKey(
@@ -34,6 +41,14 @@ export class CashierHomeComponent implements OnInit {
     this.patientUuid$ = this.systemSettingsService.getSystemSettingsByKey(
       `iCareConnect.pharmacy.patientUuid`
     );
+    this.encounterTypeUuid$ = this.systemSettingsService.getSystemSettingsByKey(
+      `iCareConnect.pharmacy.encounterTypeUuid`
+    );
+    this.prescriptionVariables$ =
+      this.systemSettingsService.getSystemSettingsByKey(
+        `iCare.clinic.prescription.arrangement`
+      );
+
     this.currentLocation = localStorage.getItem(`currentLocation`)
       ? JSON.parse(localStorage.getItem(`currentLocation`))
       : null;
