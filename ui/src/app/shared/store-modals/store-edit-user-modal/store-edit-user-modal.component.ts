d8e7ed2d6aa100f0c21e99f6784e18e46ee26f51
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-01-18 20:18:58 +0300

    Add support to add new user and edit existing one

--- ui/src/app/shared/store-modals/store-edit-user-modal/store-edit-user-modal.component.ts
+++ ui/src/app/shared/store-modals/store-edit-user-modal/store-edit-user-modal.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Observable } from "rxjs";
+import { SystemSettingsService } from "src/app/core/services/system-settings.service";
 
 @Component({
-  selector: 'app-store-edit-user-modal',
-  templateUrl: './store-edit-user-modal.component.html',
-  styleUrls: ['./store-edit-user-modal.component.scss']
+  selector: "app-store-edit-user-modal",
+  templateUrl: "./store-edit-user-modal.component.html",
+  styleUrls: ["./store-edit-user-modal.component.scss"],
 })
 export class StoreEditUserModalComponent implements OnInit {
-
-  constructor() { }
+  securitySystemSettings$: Observable<any>;
+  constructor(
+    private dialogRef: MatDialogRef<StoreEditUserModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private systemSettingsService: SystemSettingsService
+  ) {}
 
   ngOnInit(): void {
+    this.securitySystemSettings$ =
+      this.systemSettingsService.getSystemSettingsMatchingAKey("security.");
   }
 
+  onClose(event: Event): void {
+    event.stopPropagation();
+    this.dialogRef.close();
+  }
+
+  onCancel(shouldClose: boolean): void {
+    if (shouldClose) {
+      this.dialogRef.close();
+    }
+  }
 }
