0a1b5a1a7e99868a053dfa662e107a960c44a684
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-01-18 18:39:43 +0300

    Add support to set confirmation diaglog

--- ui/src/app/modules/pharmacy/modules/cashier/components/encounters-list/encounters-list.component.ts
+++ ui/src/app/modules/pharmacy/modules/cashier/components/encounters-list/encounters-list.component.ts
@@ -49,7 +49,7 @@ export class EncountersListComponent implements OnInit {
             ...response,
             results: response?.results?.map((result: any, index: number) => {
               let data = {};
-              data["no"] = index + 1;
+              data["no"] = index + 1 + this.startIndex;
               // Object.keys();
               this.customForm?.formFields.forEach((field: any) => {
                 const observations = result.obs?.map((observation: any) => {
@@ -74,5 +74,14 @@ export class EncountersListComponent implements OnInit {
         this.encounters = encountersData?.results;
       }
     });
+  }
+
+  onGetList(event: Event, actionType: string): void {
+    event.stopPropagation();
+    this.startIndex =
+      actionType === "next"
+        ? this.startIndex + 1 + this.limit
+        : this.startIndex - this.limit - 1;
+    this.loadEncounters();
   }
 }
