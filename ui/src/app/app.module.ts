66d85f5c5bff8743bbfc2e6d75ef95eee6680b07
Helga0205 <helgageorge133@gmail.com>
2024-01-24 12:04:16 +0300

    Add functionality feature to remove caches in the system

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core";
+//Import services to app.module.ts
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NoCacheInterceptor } from "./non_cache.inteceptor";
+import { browser } from "protractor";
 export const config: any = {
   sizeUnit: "Octet",
 };
 
 @NgModule({
   declarations: [AppComponent],
+//use services imported
   imports: [
+    BrowserModule, HttpClientModule,
     CoreModule.forRoot({
       namespace: "icare",
       version: 1,
@@ -16,7 +23,15 @@ export const config: any = {
       },
     }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass:NoCacheInterceptor, 
+      multi:true,
+    },
+  ],
+
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
