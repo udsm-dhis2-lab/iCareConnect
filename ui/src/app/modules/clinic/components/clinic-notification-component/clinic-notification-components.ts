a5ca938683b55ac1be488f2d372b42af346630a8
Henry Challo <154272059+Henrychallooo@users.noreply.github.com>
2024-01-26 17:39:48 +0300

    Create clinic-notification-components.ts

---
+++ ui/src/app/modules/clinic/components/clinic-notification-component/clinic-notification-components.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NotificationService } from "src/app/shared/services/notification.service";
+
+@Component({
+  selector: 'app-clinic-notification',
+  template: `
+    <div *ngFor="let notification of notifications">
+      {{ notification }}
+    </div>
+  `,
+})
+export class ClinicNotificationComponent implements OnInit, OnDestroy {
+  notifications: string[] = [];
+  private subscription: Subscription;
+
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit() {
+    // Subscribe to the notification observable
+    this.subscription = this.notificationService.notification$.subscribe(
+      (message) => {
+        // Add the notification to the array
+        this.notifications.push(message);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe to avoid memory leaks
+    this.subscription.unsubscribe();
+  }
+}
