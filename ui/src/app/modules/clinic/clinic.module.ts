568a57d04026288dc3d9e82218830f9faf778d65
auditarimo <131449668+auditarimo@users.noreply.github.com>
2024-01-26 17:10:53 +0300

    Update clinic.module.ts

--- ui/src/app/modules/clinic/clinic.module.ts
+++ ui/src/app/modules/clinic/clinic.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClinicNotificationComponent } from './components/clinic-notification-component';
 
 import { ClinicRoutingModule } from './clinic-routing.module';
 import { clinicPages } from './pages';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [...clinicPages],
+  declarations: [...clinicPages, ClinicNotificationComponent],
   providers: [],
   imports: [CommonModule, ClinicRoutingModule, SharedModule],
 })
