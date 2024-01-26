46c113340720b024a2e90bbc6743e84aba5ec118
RosemaryCharles <rosemarycharles224@gmail.com>
2024-01-26 11:19:55 +0300

    add consumables button

--- ui/src/app/shared/components/patient-history/patient-history.component.ts
+++ ui/src/app/shared/components/patient-history/patient-history.component.ts
@@ -39,10 +39,7 @@ export class PatientHistoryComponent implements OnInit {
     username?: string;
     systemId?: string;
     userProperties?: object;
-    person?: import("/home/masembo/Project/DHIS2/NEW ICARE/icare/ui/src/app/shared/resources/openmrs").PersonGetRef;
-    privileges?: import("/home/masembo/Project/DHIS2/NEW ICARE/icare/ui/src/app/shared/resources/openmrs").PrivilegeGetRef[];
-    roles?: import("/home/masembo/Project/DHIS2/NEW ICARE/icare/ui/src/app/shared/resources/openmrs").RoleGetRef[];
-    provider?: { uuid?: string; display?: string };
+   provider?: { uuid?: string; display?: string };
   }>;
   constructor(
     private visitsService: VisitsService,
