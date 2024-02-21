8f401139250f458bba9acf8f14bc0fc08b6808d8
Dkiba <kibahiladennis@gmail.com>
2024-02-21 20:33:07 +0300

    fix audit log api issue

--- omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/BaseController.java
+++ omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/BaseController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.transaction.Transactional;
 import java.util.Date;
 
-public class BaseController implements UserSessionListener {
+public class BaseController {
 	
 	/*protected final Log log = LogFactory.getLog(getClass());
 	
@@ -29,26 +29,4 @@ public class BaseController implements UserSessionListener {
 		return errorMessage;
 	}*/
 	
-	@Autowired
-	ICareService iCareService;
-	
-	@Override
-	@Transactional
-	public void loggedInOrOut(User user, Event event, Status status) {
-		
-		if (event == Event.LOGIN) {
-			// User logged in successfully
-			System.out.println("User " + user.getUsername() + " logged in at " + new Date() + " and status " + event);
-			AuditLog auditLog = new AuditLog(User.class, "LOGGED IN", user, new Date());
-			iCareService.saveAuditLog(auditLog);
-			// Log the event or perform other actions
-		} else if (event == Event.LOGOUT) {
-			// User logged out successfully
-			System.out.println("User " + user.getUsername() + " logged out at " + new Date() + " and event " + event);
-			AuditLog auditLog = new AuditLog(User.class, "LOGGED OUT", user, new Date());
-			iCareService.saveAuditLog(auditLog);
-			
-		}
-		
-	}
 }
