2754f70787d6deabc2fc0ef14ea46697b686584b
Dkiba <kibahiladennis@gmail.com>
2024-02-21 18:58:38 +0300

    fix resource issue for api v1/icare

--- omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/BaseController.java
+++ omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/BaseController.java
@@ -1,6 +1,15 @@
 package org.openmrs.module.icare.web.controller;
 
-public class BaseController {
+import org.openmrs.User;
+import org.openmrs.UserSessionListener;
+import org.openmrs.module.icare.auditlog.AuditLog;
+import org.openmrs.module.icare.core.ICareService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+
+public class BaseController implements UserSessionListener {
 	
 	/*protected final Log log = LogFactory.getLog(getClass());
 	
@@ -19,4 +28,27 @@ public class BaseController {
 		errorMessage.put("details", sStackTrace);
 		return errorMessage;
 	}*/
+	
+	@Autowired
+	ICareService iCareService;
+	
+	@Override
+	@Transactional
+	public void loggedInOrOut(User user, Event event, Status status) {
+		
+		if (event == Event.LOGIN) {
+			// User logged in successfully
+			System.out.println("User " + user.getUsername() + " logged in at " + new Date() + " and status " + event);
+			AuditLog auditLog = new AuditLog(User.class, "LOGGED IN", user, new Date());
+			iCareService.saveAuditLog(auditLog);
+			// Log the event or perform other actions
+		} else if (event == Event.LOGOUT) {
+			// User logged out successfully
+			System.out.println("User " + user.getUsername() + " logged out at " + new Date() + " and event " + event);
+			AuditLog auditLog = new AuditLog(User.class, "LOGGED OUT", user, new Date());
+			iCareService.saveAuditLog(auditLog);
+			
+		}
+		
+	}
 }
