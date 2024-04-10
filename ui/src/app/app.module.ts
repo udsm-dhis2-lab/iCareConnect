0aa56480902d587fdb6584af25e7e4e819b05a20
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-04-10 07:40:32 +0300

    Fix dependency issue with angulartics2

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core";
 import { BrowserModule } from "@angular/platform-browser";
-import { Angulartics2Module } from 'angulartics2';
-import { Angulartics2GoogleAnalytics } from 'angulartics2';
+import { Angulartics2Module } from "angulartics2";
 
 export const config: any = {
   sizeUnit: "Octet",
@@ -20,7 +19,7 @@ export const config: any = {
       },
     }),
     BrowserModule,
-    Angulartics2Module.forRoot(),
+    Angulartics2Module.forRoot({ developerMode: false }),
   ],
   providers: [],
   bootstrap: [AppComponent],
