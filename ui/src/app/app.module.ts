f90102eb82ba3403238ab38d9ecc09a9f80e7687
Dkiba <kibahiladennis@gmail.com>
2024-05-15 18:04:26 +0300

    add pwa configurations to support responsiveness and user experience

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core";
 import { BrowserModule } from "@angular/platform-browser";
 import { GoogleAnalyticsService } from "./google-analytics.service";
+import { ServiceWorkerModule } from '@angular/service-worker';
 export const config: any = {
   sizeUnit: "Octet",
 };
@@ -18,6 +19,12 @@ export const config: any = {
       },
     }),
     BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     GoogleAnalyticsService,
