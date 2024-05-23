296765bb1f350ad86f654863a3401258f49c98bf
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-05-23 17:29:11 +0300

    Add support to post data to configuired mediators

--- omods/core/api/src/main/java/org/openmrs/module/icare/core/aop/VisitDiagnosisAdvisor.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/core/aop/VisitDiagnosisAdvisor.java
@@ -15,6 +15,7 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.icare.ICareConfig;
 import org.openmrs.module.icare.core.ICareService;
 import org.openmrs.module.icare.core.utils.Dhis2EventWrapper;
+import org.openmrs.module.icare.core.utils.EidsrWrapper;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
 
@@ -60,7 +61,7 @@ public class VisitDiagnosisAdvisor extends StaticMethodMatcherPointcutAdvisor im
 				}
                 if (diagnosis.getDateCreated() == null && shouldCaptureDiagnosis.equals("true") && codesReferenceArray.length() > 0) {
 						// Logics to push data to external system
-
+						Date todayDate = new Date();
 						String diagnosisName = diagnosis.getDiagnosis().getCoded().getDisplayString();
 						String icdCode = "";
 						Matcher matches = Pattern.compile("\\((.*?)\\)").matcher(diagnosisName);
@@ -84,28 +85,73 @@ public class VisitDiagnosisAdvisor extends StaticMethodMatcherPointcutAdvisor im
 							}
 							capturedEventData.put("identifier", patientIdentifier);
 							capturedEventData.put("gender",diagnosis.getPatient().getGender().toString());
+							capturedEventData.put("reportingDate",dhis2EventWrapper.formatDateToYYYYMMDD(todayDate));
+							capturedEventData.put("eventDate",dhis2EventWrapper.formatDateToYYYYMMDD(todayDate));
+							capturedEventData.put("visitNumber",diagnosis.getEncounter().getVisit().getVisitId());
+							if (diagnosis.getPatient().getPerson().getDead().booleanValue() == true ) {
+								capturedEventData.put("isPatientAlive", "No");
+							} else {
+								capturedEventData.put("isPatientAlive", "Yes");
+							}
+
 							capturedEventData.put("dob",dhis2EventWrapper.formatDateToYYYYMMDD(diagnosis.getPatient().getPerson().getBirthdate()));
 							capturedEventData.put("firstName",diagnosis.getPatient().getGivenName().toString());
 							capturedEventData.put("certainty", diagnosis.getCertainty().toString());
 							capturedEventData.put("surname",diagnosis.getPatient().getFamilyName().toString());
 							capturedEventData.put("diagnosis", diagnosisName);
-							AdministrationService adminService = Context.getService(AdministrationService.class);
-							String mappings = adminService.getGlobalProperty(ICareConfig.SURVEILLANCE_SINGLE_EVENT_PROGRAM_MAPPINGS);
-							Map<String, Object> event = new HashMap<>();
-							event.put("orgUnit", dhis2EventWrapper.getHFRCode());
-							event.put("programStage", dhis2EventWrapper.getEventProgramStage());
-							event.put("program", dhis2EventWrapper.getEventProgram());
-							Date todayDate = new Date();
-							event.put("occurredAt", dhis2EventWrapper.formatDateToYYYYMMDD(todayDate));
-							event.put("dataValues", dhis2EventWrapper.formulateDataValuesObject(capturedEventData,mappings));
-							//			eventData.put("residence",diagnosis.getPatient().getAddresses());
-							Map<String, Object> eventsPayload = new HashMap<>();
-							List<Map<String, Object>> events = new ArrayList<>();
-							events.add(event);
-							eventsPayload.put("events", events);
-
+							capturedEventData.put("diseaseCode", icdCode);
+							String facilityCode = dhis2EventWrapper.getHFRCode();
+							capturedEventData.put("facilityCode", facilityCode);
 							ICareService iCareService = Context.getService(ICareService.class);
-							String response = iCareService.pushEventWithoutRegistrationDataToDHIS2Instance(new JSONObject(eventsPayload).toString());
+							AdministrationService adminService = Context.getService(AdministrationService.class);
+							String mediatorsConfigs = adminService.getGlobalProperty(ICareConfig.INTEROPERABILITY_MEDIATORS_LIST);
+							JSONArray mediatorsList = new JSONArray(mediatorsConfigs);
+							for (int count = 0; count < mediatorsList.length(); count++) {
+								JSONObject mediator = mediatorsList.getJSONObject(count);
+								if (mediator.has("isActive") && mediator.getBoolean("isActive")) {
+									if (mediator.has("mediatorKey") && mediator.getString("mediatorKey").equals("dhis2")) {
+										String mappings = adminService.getGlobalProperty(ICareConfig.SURVEILLANCE_SINGLE_EVENT_PROGRAM_MAPPINGS);
+										Map<String, Object> event = new HashMap<>();
+										event.put("orgUnit", facilityCode);
+										event.put("programStage", dhis2EventWrapper.getEventProgramStage());
+										event.put("program", dhis2EventWrapper.getEventProgram());
+										event.put("occurredAt", dhis2EventWrapper.formatDateToYYYYMMDD(todayDate));
+										event.put("dataValues", dhis2EventWrapper.formulateDataValuesObject(capturedEventData,mappings));
+										//			eventData.put("residence",diagnosis.getPatient().getAddresses());
+										Map<String, Object> eventsPayload = new HashMap<>();
+										List<Map<String, Object>> events = new ArrayList<>();
+										events.add(event);
+										eventsPayload.put("events", events);
+
+										String response = iCareService.pushEventWithoutRegistrationDataToDHIS2Instance(new JSONObject(eventsPayload).toString());
+
+										GlobalProperty globalProperty = new GlobalProperty();
+										globalProperty.setProperty("surveillance.test.dhis");
+										globalProperty.setPropertyValue(response);
+										adminService.saveGlobalProperty(globalProperty);
+									} else if (mediator.has("mediatorKey") && mediator.getString("mediatorKey").equals("eidsr") && mediator.has("mediatorMappingReferenceKey") && mediator.has("mediatorUrlPath")) {
+
+										String mediatorMappingReferenceKey = mediator.getString("mediatorMappingReferenceKey");
+										String mediatorKey = mediator.getString("mediatorKey");
+										String mediatorUrlPath = mediator.getString("mediatorUrlPath");
+										String authenticationType = mediator.getString("authenticationType");
+										String mappings = adminService.getGlobalProperty(mediatorMappingReferenceKey);
+
+										if (mediatorUrlPath != null && mappings != null) {
+											EidsrWrapper eidsrWrapper = new EidsrWrapper();
+											Map<String, Object> data = eidsrWrapper.formatData(mappings,capturedEventData);
+
+											if (new JSONObject(data).toString() != null) {
+												String response = iCareService.pushDataToExternalMediator(new JSONObject(data).toString(),mediatorKey,mediatorUrlPath,authenticationType);
+												GlobalProperty globalProperty2 = new GlobalProperty();
+												globalProperty2.setProperty("surveillance.test.eidsrMediator.response");
+												globalProperty2.setPropertyValue(response);
+												adminService.saveGlobalProperty(globalProperty2);
+											}
+										}
+									}
+								}
+							}
 						}
 					}
 				}
