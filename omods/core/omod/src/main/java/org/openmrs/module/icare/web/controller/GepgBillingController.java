4e8bdc61aa90004fee0fa2b65af5a6f06a131656
gaspar-dev <kalugendosmazima@gmail.com>
2024-05-26 01:02:32 +0300

    gepg intergration test

--- omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/GepgBillingController.java
+++ omods/core/omod/src/main/java/org/openmrs/module/icare/web/controller/GepgBillingController.java
@@ -1,20 +1,26 @@
 package org.openmrs.module.icare.web.controller;
 
 import org.openmrs.module.icare.billing.services.payment.gepg.GEPGService;
+import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.icare.billing.services.payment.gepg.BillSubmissionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/gepg")
+@RequestMapping("/api/"+RestConstants.VERSION_1 +"/gepg")
 public class GepgBillingController {
 
     @Autowired
     GEPGService gepgbillService;
 
-    @RequestMapping(value = "/submit", method = RequestMethod.POST)
+    public GepgBillingController(RestTemplate restTemplate) {
+        //TODO Auto-generated constructor stub
+    }
+
+    @RequestMapping(value = "/controlNumber", method = RequestMethod.POST)
     public String submitBill(@RequestBody Map<String, String> payload) {
         String uuid = payload.get("uuid");
 
