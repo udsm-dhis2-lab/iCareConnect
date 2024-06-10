d4cc54fbde50c404f0cfeb0ff3b0794e7e4709cb
gaspar-dev <kalugendosmazima@gmail.com>
2024-06-10 09:33:47 +0300

    json formulated

--- omods/core/api/src/main/java/org/openmrs/module/icare/billing/services/payment/gepg/BillItem.java
+++ omods/core/api/src/main/java/org/openmrs/module/icare/billing/services/payment/gepg/BillItem.java
@@ -1,82 +1,76 @@
 package org.openmrs.module.icare.billing.services.payment.gepg;
 
 public class BillItem {
-    private String ItemId;
-    private String IsqBillItemId;
-    private String BillItemAmt;
-    private String EqvAmt;
-    private String MiscAmt;
-    private String PymtOpt;
-
-    // Constructor
-    public BillItem(String ItemId, String IsqBillItemId, String BillItemAmt, String EqvAmt, String MiscAmt, String PymtOpt) {
-        this.ItemId = ItemId;
-        this.IsqBillItemId = IsqBillItemId;
-        this.BillItemAmt = BillItemAmt;
-        this.EqvAmt = EqvAmt;
-        this.MiscAmt = MiscAmt;
-        this.PymtOpt = PymtOpt;
-    }
-
-    // Getter Methods
-    public String getItemId() {
-        return ItemId;
-    }
-
-    public String getIsqBillItemId() {
-        return IsqBillItemId;
-    }
-
-    public String getBillItemAmt() {
-        return BillItemAmt;
-    }
-
-    public String getEqvAmt() {
-        return EqvAmt;
-    }
-
-    public String getMiscAmt() {
-        return MiscAmt;
-    }
-
-    public String getPymtOpt() {
-        return PymtOpt;
-    }
-
-    // Setter Methods
-    public void setItemId(String ItemId) {
-        this.ItemId = ItemId;
-    }
-
-    public void setIsqBillItemId(String IsqBillItemId) {
-        this.IsqBillItemId = IsqBillItemId;
-    }
-
-    public void setBillItemAmt(String BillItemAmt) {
-        this.BillItemAmt = BillItemAmt;
-    }
-
-    public void setEqvAmt(String EqvAmt) {
-        this.EqvAmt = EqvAmt;
-    }
-
-    public void setMiscAmt(String MiscAmt) {
-        this.MiscAmt = MiscAmt;
-    }
-
-    public void setPymtOpt(String PymtOpt) {
-        this.PymtOpt = PymtOpt;
-    }
-
-    @Override
-    public String toString() {
-        return "BillItem{" +
-                "ItemId='" + ItemId + '\'' +
-                ", IsqBillItemId='" + IsqBillItemId + '\'' +
-                ", BillItemAmt='" + BillItemAmt + '\'' +
-                ", EqvAmt='" + EqvAmt + '\'' +
-                ", MiscAmt='" + MiscAmt + '\'' +
-                ", PymtOpt='" + PymtOpt + '\'' +
-                '}';
-    }
+	
+	private String billItemRef;
+	
+	private String useItemRefOnPay;
+	
+	private String billItemAmt;
+	
+	private String billItemEqvAmt;
+	
+	private String billItemMiscAmt;
+	
+	private String gfsCode;
+	
+	// Constructor
+	public BillItem(String billItemRef, String useItemRefOnPay, String billItemAmt, String billItemEqvAmt,
+	    String billItemMiscAmt, String gfsCode) {
+		this.billItemRef = billItemRef;
+		this.useItemRefOnPay = useItemRefOnPay;
+		this.billItemAmt = billItemAmt;
+		this.billItemEqvAmt = billItemEqvAmt;
+		this.billItemMiscAmt = billItemMiscAmt;
+		this.gfsCode = gfsCode;
+	}
+	
+	// Getters and setters
+	public String getBillItemRef() {
+		return billItemRef;
+	}
+	
+	public void setBillItemRef(String billItemRef) {
+		this.billItemRef = billItemRef;
+	}
+	
+	public String getUseItemRefOnPay() {
+		return useItemRefOnPay;
+	}
+	
+	public void setUseItemRefOnPay(String useItemRefOnPay) {
+		this.useItemRefOnPay = useItemRefOnPay;
+	}
+	
+	public String getBillItemAmt() {
+		return billItemAmt;
+	}
+	
+	public void setBillItemAmt(String billItemAmt) {
+		this.billItemAmt = billItemAmt;
+	}
+	
+	public String getBillItemEqvAmt() {
+		return billItemEqvAmt;
+	}
+	
+	public void setBillItemEqvAmt(String billItemEqvAmt) {
+		this.billItemEqvAmt = billItemEqvAmt;
+	}
+	
+	public String getBillItemMiscAmt() {
+		return billItemMiscAmt;
+	}
+	
+	public void setBillItemMiscAmt(String billItemMiscAmt) {
+		this.billItemMiscAmt = billItemMiscAmt;
+	}
+	
+	public String getGfsCode() {
+		return gfsCode;
+	}
+	
+	public void setGfsCode(String gfsCode) {
+		this.gfsCode = gfsCode;
+	}
 }
