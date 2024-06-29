5fe129b1a1317774ac6e8c6f51566e2efd18a9b9
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-06-29 16:32:13 +0300

    Add API for generating control number from invoice

--- omods/core/omod/src/test/java/org/openmrs/module/icare/web/controller/GepgControllerAPITest.java
+++ omods/core/omod/src/test/java/org/openmrs/module/icare/web/controller/GepgControllerAPITest.java
@@ -2,7 +2,9 @@ package org.openmrs.module.icare.web.controller;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.After;
 import org.junit.Before;
 
@@ -14,6 +16,8 @@ import org.openmrs.module.icare.billing.models.Invoice;
 import org.openmrs.module.icare.billing.services.BillingService;
 import org.openmrs.module.icare.web.controller.core.BaseResourceControllerTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import static org.junit.Assert.assertTrue;
 
@@ -36,17 +40,30 @@ public class GepgControllerAPITest extends BaseResourceControllerTest {
 		this.clearSessionAfterEachTest();
 	}
 	
-	 @Test
-	 public void testSubmitBill_withValidUuid_returnsSuccess() {
+	@Test
+	public void testSubmitBill_withValidUuid_returnsSuccess() {
 		//Given
 		PatientService patientService = Context.getService(PatientService.class);
 		Patient patient = patientService.getPatientByUuid("1f6959e5-d15a-4025-bb48-340ee9e2c58d");
-	
+		
 		List<Invoice> invoices = billingService.getPendingInvoices(patient.getUuid());
 		Invoice invoice = invoices.get(0);
 		System.out.println("invoice data -------------------------");
 		System.out.println(invoice);
-	 	System.out.println("Test function fired ------------");
-	 	assertTrue(true);
-	 }
+		System.out.println("Test function fired ------------");
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGenerateControlNumber() throws Exception {
+		// Given
+		String invoices = this.readFile("dto/billing/generate-control-number-by-invoice.json");
+		List invoicesList = (new ObjectMapper()).readValue(invoices, List.class);
+		// When
+		MockHttpServletRequest mockRequest = newPutRequest("gepg/generatecontrolno", invoicesList);
+		MockHttpServletResponse generateControlNumberRequest = handle(mockRequest);
+		System.out.println(generateControlNumberRequest.getContentAsString());
+		// Then
+		// TODO: Implement test results
+	}
 }
