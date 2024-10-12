2db8bc6ca589807ede15e35656f5a8850f273ea6
Josephat Mwakyusa <josephatjulius24@gmail.com>
2024-10-12 10:50:48 +0300

    Add remote history modal boilerplate

--- ui/src/app/shared/dialogs/shared-remote-patient-history-modal/shared-remote-patient-history-modal.component.ts
+++ ui/src/app/shared/dialogs/shared-remote-patient-history-modal/shared-remote-patient-history-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { OpenmrsHttpClientService } from "../../modules/openmrs-http-client/services/openmrs-http-client.service";
 
 @Component({
   selector: "app-shared-remote-patient-history-modal",
@@ -7,9 +8,17 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrl: "./shared-remote-patient-history-modal.component.scss",
 })
 export class SharedRemotePatientHistoryModalComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private httpClientService: OpenmrsHttpClientService
+  ) {}
 
   ngOnInit(): void {
     console.log(this.data);
+    this.httpClientService
+      .get("icare/sharedRecords")
+      .subscribe((response: any) => {
+        console.log(response);
+      });
   }
 }
