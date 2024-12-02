cad4f2e6c5a758df1be6caaaad032c863f990880
gaspar-dev <kalugendosmazima@gmail.com>
2024-12-02 10:51:09 +0300

    Added support to ensure GePG Request handless error from live

--- ui/src/app/shared/resources/visits/services/visits.service.ts
+++ ui/src/app/shared/resources/visits/services/visits.service.ts
@@ -357,7 +357,7 @@ export class VisitsService {
     }
     //
     
-    console.log("url fired  ...",`icare/visit?${parametersString.replace("&","" )}&startIndex=${startIndex}&limit=${limit}`);
+    // console.log("url fired  ...",`icare/visit?${parametersString.replace("&","" )}&startIndex=${startIndex}&limit=${limit}`);
     return (
       locationUuids?.length > 0
         ? zip(
