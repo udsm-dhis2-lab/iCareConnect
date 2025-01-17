62292ce09071db5dfd576b6db4a9b4541f52c410
Emmanuel Cyprian <emmanuelcyprian268@gmail.com>
2025-01-17 12:18:03 +0300

    uncomment ngonInit()  logic and  this store dipatch

--- ui/src/app/core/containers/modules/modules.component.ts
+++ ui/src/app/core/containers/modules/modules.component.ts
@@ -39,7 +39,7 @@ export class ModulesComponent implements OnInit {
   constructor(private store: Store<AppState>, private router: Router) {}
 
   ngOnInit() {
-    /*this.currentLocation$ = this.store.select(getCurrentLocation(false));
+    this.currentLocation$ = this.store.select(getCurrentLocation(false));
     this.store.dispatch(loadRolesDetails());
     this.searchModules();
 
@@ -61,7 +61,7 @@ export class ModulesComponent implements OnInit {
         // NB: Navigation has to be moved to module selector
         this.navigateToApp(assignedApps[0].path);
       }
-    });*/
+    });
   }
 
   navigateToApp(path): void {
@@ -90,9 +90,9 @@ export class ModulesComponent implements OnInit {
       /**
        * Navigation has been moved to module selector
        */
-      // this.store.dispatch(
-      //   go({ path: [basePath], extras: { queryParams: formattedParam } })
-      // );
+      this.store.dispatch(
+       go({ path: [basePath], extras: { queryParams: formattedParam } })
+       );
     }
   }
 
