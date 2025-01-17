89fbb819940da3d45a0a537a20f57a9c850cc064
FeruzyNtillah <feyrouz10@icloud.com>
2025-01-17 10:18:34 +0300

    i added the console log in the pipe to verify that the data is being fetched and structured as expected.

--- ui/src/app/shared/components/patient-history/patient-history.component.ts
+++ ui/src/app/shared/components/patient-history/patient-history.component.ts
@@ -147,6 +147,7 @@ export class PatientHistoryComponent implements OnInit {
       .pipe(
         map((response) => {
           this.loadingData = false;
+          console.log('Fetched visits:',response);
           if (!response?.error) {
             return response?.map((visit) => {
               let obs = [];
