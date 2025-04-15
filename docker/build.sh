9d0cebe5b1c3465326a032f7eecc181e05a24f07
Bennett Benedict <bennybenester@gmail.com>
2025-04-15 15:33:35 +0300

    release: Improve getting branch version tag

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,14 @@
 sh docker/build-local.sh
 version=$(cat version)
-branch=$(git branch | grep \* | cut -d ' ' -f2)
+# branch=$(git branch | grep \* | cut -d ' ' -f2)
+# branch=$(git branch | grep \* | cut -d ' ' -f2 | sed 's/[^a-zA-Z0-9._-]/_/g')
 # docker push udsmdhis2/icare-core:local
-docker push udsmdhis2/icare-core:$branch-$version
+if [ -n "$GITHUB_HEAD_REF" ]; then
+  branch="$GITHUB_HEAD_REF"
+else
+  branch="${GITHUB_REF##*/}"
+fi
+safe_branch=$(echo "$branch" | sed 's/[^a-zA-Z0-9._-]/_/g')
+
+docker push udsmdhis2/icare-core:$safe_branch-$version
+# docker push udsmdhis2/icare-core:$branch-$version
