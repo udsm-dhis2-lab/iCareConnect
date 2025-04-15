291b8c7e6579259f6166eaf6c878304c278e1518
Bennett Benedict <bennybenester@gmail.com>
2025-04-15 13:49:39 +0300

    release: Remove hardcoded NHIF credentials

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  NHIF_CLIENT_ID: "60c8a892f36f",
+  NHIF_CLIENT_SECRET: "60c8a892f36f/I5A==",
+  NHIF_USERNAME: "60c8a892f36f",
+  NHIF_SCOPE: "60c8a892f36f",
 };
