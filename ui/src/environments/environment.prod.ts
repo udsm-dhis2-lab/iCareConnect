bcec5bff68a64b4b977fdac792e4f431f52692b1
Bennett Benedict <bennybenester@gmail.com>
2025-04-22 18:17:02 +0300

    release: Fix error getting openmrs context

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   NHIF_CLIENT_ID: "60c8a892f36f",
   NHIF_CLIENT_SECRET: "60c8a892f36f/I5A==",
   NHIF_USERNAME: "60c8a892f36f",
-  NHIF_SCOPE: "60c8a892f36f",
+  NHIF_SCOPE: "60c8a892f36ff",
 };
