0e3935dec7c5cec46fdf79a5d239924e08940579
Jonas1015 <jonasgsmart1015@gmail.com>
2025-09-03 15:24:12 +0300

    Add checks and improvements for lab module login

--- ui/src/app/core/containers/landing/app-landing.component.ts
+++ ui/src/app/core/containers/landing/app-landing.component.ts
@@ -13,7 +13,6 @@ import { getLISConfigurations } from "src/app/store/selectors/lis-configurations
   styleUrls: ["./app-landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
-  // LISConfigurations$: Observable<any>;
   LISConfigurations;
   locationsForCurrentUser$: Observable<any[]>;
   constructor(private store: Store<AppState>) {}
@@ -22,7 +21,6 @@ export class LandingComponent implements OnInit {
     this.store.dispatch(loadLISConfigurations());
     this.locationsForCurrentUser$ = this.store.select(getUserAssignedLocations);
 
-    // Get the navigation details from localstorage
     const navigationDetails = JSON.parse(
       localStorage.getItem("navigationDetails")
     );
@@ -30,28 +28,6 @@ export class LandingComponent implements OnInit {
       !navigationDetails || !navigationDetails?.path[0] ? false : true;
     
     await this.getLISConfigurations()
-
-    // this.LISConfigurations$ = this.store.select(getLISConfigurations).pipe(takeLast(1),
-    //   tap((response) => {
-    //     if (response && response?.isLIS) {
-    //       this.store.dispatch(
-    //         go({
-    //           path: isNavigationDetailsAvailable
-    //             ? navigationDetails?.path
-    //             : ["/laboratory/dashboard-lab"],
-    //         })
-    //       );
-    //     } else if (response?.isPharmacy) {
-    //       this.store.dispatch(
-    //         go({
-    //           path: isNavigationDetailsAvailable
-    //             ? navigationDetails?.path
-    //             : ["/pharmacy/home"],
-    //         })
-    //       );
-    //     }
-    //   })
-    // );
   }
 
   async getLISConfigurations() {
